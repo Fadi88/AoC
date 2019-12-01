cadff6a88922acb86e1831e20a25c8f24fadbf21
Fadi Sedik <fady88@gmail.com>
2019-12-01 13:54:34 +0100

    adding final solution

--- day1/day1-1.cpp
+++ day1/day1-1.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main() {
 
-	std::ofstream input_fd;
-	input_fd.open('input/day1-1_input.txt' , 'r');
 
+	std::ifstream input_fd;
+	input_fd.open("input/day1-1_input.txt");
 
+	uint32_t sum{}, tmp_val{};
+
+	while (input_fd >> tmp_val) {
+
+		sum += ((tmp_val / 3) - 2);
+	}
+
+	input_fd.close();
+
+	std::cout << "sum of fuel mass is : " << sum << std::endl;
 	return 0;
 }
