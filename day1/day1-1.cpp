a13e213c1bc0e1d054fd8dcca0f7befd4ef8cc96
Your Name <you@example.com>
2019-12-01 15:23:40 +0100

    moving to visual studio

--- day1/day1-1.cpp
+++ day1/day1-1.cpp
@@ -6,13 +6,14 @@ int main() {
 
 
 	std::ifstream input_fd;
-	input_fd.open("input/day1-1_input.txt");
+	input_fd.open("input\\day1_input.txt");
 
 	uint32_t sum{}, tmp_val{};
 
 	while (input_fd >> tmp_val) {
 
 		sum += ((tmp_val / 3) - 2);
+		std::cout << tmp_val << std::endl;
 	}
 
 	input_fd.close();
