0ec26287dc90e54eb58012f33c80888db6deb220
Fadi Sedik <fady88@gmail.com>
2019-12-01 14:47:55 +0100

    adding second part

---
+++ day1/day1-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+uint32_t getFuelReq(uint32_t mass){
+	return  mass / 3 > 2  ? ((mass / 3) - 2) : 0;
+}
+
+int main() {
+
+	std::ifstream input_fd;
+	input_fd.open("input/day1-1_input.txt");
+
+	uint32_t sum{}, input_val{};
+
+	while (input_fd >> input_val) {
+		while (input_val / 3 > 0 ) {
+			uint32_t tmp_val = getFuelReq(input_val);
+			sum += tmp_val;
+			input_val = tmp_val;
+		}
+	}
+
+	input_fd.close();
+
+	std::cout << "sum of fuel mass is : " << sum << std::endl;
+	return 0;
+}
