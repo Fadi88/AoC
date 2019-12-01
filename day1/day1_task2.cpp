6175af7d120413f2f64004519ceb43ae19dcb7ad
Fadi Sedik <fady88@gmail.com>
2019-12-01 18:24:51 +0100

    use RAII with basic_ifstream

--- day1/day1_task2.cpp
+++ day1/day1_task2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 uint32_t getFuelReq(uint32_t mass){
 	return  mass / 3 > 2  ? ((mass / 3) - 2) : 0;
@@ -8,9 +7,7 @@ uint32_t getFuelReq(uint32_t mass){
 
 int main() {
 
-	std::ifstream input_fd;
-	input_fd.open("input\\day1_input.txt");
-
+	std::ifstream input_fd{ "input\\day1_input.txt" };
 	int32_t sum{}, input_val{};
 
 	while (input_fd >> input_val) {
@@ -20,8 +17,6 @@ int main() {
 			input_val = tmp_val;
 		}
 	}
-
-	input_fd.close();
 
 	std::cout << "sum of fuel mass is : " << sum << std::endl;
 	return 0;
