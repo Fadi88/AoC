97e9d2806037d55efb5fa2fcbec060e99fe404bc
Fadi88 <fady88@gmail.com>
2019-12-02 09:41:33 +0100

    dirty solution for task 2

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -2,16 +2,13 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include <stdexecpt>
+#include <stdexcept>
 
-std::vector<uint32_t> string2vector(std::string& input_txt) {
+std::vector<uint32_t> string2vector(std::string input_txt) {
 	std::vector<uint32_t> ret;
 	size_t pos{};
 	uint16_t cmd_instance{};
 	while ((pos = input_txt.find(',')) != std::string::npos) {
-
-		
-
 		std::stringstream{ input_txt.substr(0, pos) } >> cmd_instance;
 		ret.push_back(cmd_instance);
 		input_txt.erase(0, pos + 1);
@@ -23,10 +20,7 @@ std::vector<uint32_t> string2vector(std::string& input_txt) {
 
 void task_1(std::vector<uint32_t>& input) {
 
-	
-	
 	for (size_t idx{}; idx < input.size(); idx += 4) {
-
 		switch (input[idx]) {
 		case 1:
 			input[input[idx + 3]] = input[input[idx + 1]] + input[input[idx + 2]];
@@ -45,25 +39,33 @@ void task_1(std::vector<uint32_t>& input) {
 
 }
 
-uint32_t task_2(std::vector<uint32_t>& input){
+uint32_t task_2(std::string& input){
     uint32_t target_val{19690720};
+
 
     for(uint8_t noun{}; noun<=99 ; ++noun){
         for(uint8_t verb{}; verb<=99 ; ++verb){
-            input[1]=noun;
-            input[2]=verb;
+            
+			auto cmds = string2vector(input);
 
-            task_1(input);
-            if(input[0] == target_val){
+			cmds[1]=noun;
+            cmds[2]=verb;
+			
+			
+            task_1(cmds);
+            if(cmds[0] == target_val){
+
                 return 100 * noun + verb;
             }
+			
         }
     }
     throw std::runtime_error("target value not reachable within search space");
+
 }
 
 int main() {
-	std::ifstream input_fd{"input\\day2_input.txt"};
+	std::ifstream input_fd{"./day2_input.txt"};
 
 	std::string tmp;
 	input_fd >> tmp;
@@ -73,7 +75,8 @@ int main() {
 	cmds[2] = 2;
 	task_1(cmds);
 
-	std::cout << cmds[0];
+	std::cout << "task 1 result : " << cmds[0] << std::endl;
+	std::cout << "task 2 result : " << task_2(tmp) << std::endl;
 
 
 	return 0;
