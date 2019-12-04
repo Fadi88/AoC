8aeb7404e5aa1ad7ec19fbfdbcb079a25047d427
Fadi Sedik <fady88@gmail.com>
2019-12-04 22:13:45 +0100

    adding time measurment and testing it

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <chrono>
 
+class timer {
+public:
+	timer(std::string point) {
+		m_start_point = std::chrono::high_resolution_clock::now();
+		m_point = point;
+	}
 
+	~timer() {
+		auto endclock = std::chrono::high_resolution_clock::now();
+		auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_start_point).time_since_epoch().count();
+		auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endclock).time_since_epoch().count();
+
+		auto duration = end - start;
+		double ms = duration * 0.001;
+
+		std::cout << "time used by : " << m_point << " was : " << ms << " ms" << std::endl;
+
+	}
+
+private:
+	std::string m_point;
+	std::chrono::time_point<std::chrono::high_resolution_clock> m_start_point;
+};
 bool is_password_valid_1(const uint32_t p_input) {
 	bool is_repeated{}, is_decreasing{ true };
 
 	auto arr = std::to_string(p_input);
+
+
+	for (uint8_t idx{}; idx < 5; ++idx) {
+		if (arr[idx] > arr[idx + 1]) {
+			is_decreasing = false;
+			break;
+		}
+	}
+
+	if (!is_decreasing) return false;
 
 	for (uint8_t idx{}; idx < 5; ++idx) {
 		if (arr[idx] == arr[idx + 1]) {
@@ -15,14 +48,9 @@ bool is_password_valid_1(const uint32_t p_input) {
 		}
 	}
 
-        if(!is_repeated) return false;
+	
 
-	for (uint8_t idx{}; idx < 5; ++idx) {
-		if (arr[idx] > arr[idx + 1]) {
-			is_decreasing = false;
-			break;
-		}
-	}
+
 	
 	return is_repeated && is_decreasing;
 }
@@ -40,7 +68,7 @@ bool is_password_valid_2(const uint32_t p_input) {
 		}
 	}
 
-        if(!is_decreasing) return false;
+    if(!is_decreasing) return false;
 
 	for (auto tmp_chr : arr) {
 		
@@ -83,8 +111,15 @@ void task_2(uint32_t p_lower_bound, uint32_t p_upper_bound) {
 
 int main() {
 
-	task_1(271973, 785961);
-	task_2(271973, 785961);
+	{
+		timer t1{ "func 1" };
+		task_1(271973, 785961);
+	}
+
+	{
+		timer t1{ "func 2" };
+		task_2(271973, 785961);
+	}
 
 
 	return 0;
