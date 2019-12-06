0e14c8a67aca88f784044690e84d1ea71d68044c
Fadi88 <fady88@gmail.com>
2019-12-06 12:27:23 +0100

    solution to day 6 task 1

--- day6/day6.cpp
+++ day6/day6.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <chrono>
 #include <map>
+#include <algorithm>
 
 class timer {
 public:
@@ -28,50 +29,92 @@ private:
 	std::string m_point;
 	std::chrono::time_point<std::chrono::high_resolution_clock> m_start_point;
 };
+
 class system_t {
 
 	struct node {
 		node* ptr_center{nullptr};
+        std::string name;
+        bool is_visited;
+        uint32_t distance{};
 	};
 
 private:
 	std::map<std::string, node*> m_nodes_map;
-	std::vector<std::string> m_node_list;
+
+private:
+    void _visit_graph(){
+
+        for(auto& element : m_nodes_map){
+            if(!element.second->is_visited){
+                element.second->distance = 1 + _get_parent_distance(element.second->ptr_center);
+                element.second->is_visited = true;                
+            }else{
+                std::cout ;
+            }
+
+        }
+
+    }
+
+    uint32_t _get_parent_distance(node * ptr){
+        if(!ptr)
+            throw std::runtime_error("nullptr!");
+        if(ptr->is_visited)
+            return ptr->distance;
+        else{
+            ptr->is_visited = true;
+            ptr->distance = 1 + _get_parent_distance(ptr->ptr_center);
+            return ptr->distance;
+        }
+    
+        
+    }
+
 
 public:
 	void add_orbit(std::string p_center, std::string p_orbiter) {
 
-		node* orbiter_ptr = new node();
-		m_nodes_map[p_orbiter] = orbiter_ptr;
-		m_node_list.push_back(p_orbiter);
+        auto orbiter_node_itr = m_nodes_map.find(p_orbiter);
 
-		auto center_node_itr = std::find(m_node_list.begin(), m_node_list.end(), p_center);
+        node* orbiter_ptr{nullptr};
 
-		if (center_node_itr == m_node_list.end()) {
+        if (orbiter_node_itr == m_nodes_map.end()) {
+		    orbiter_ptr = new node();
+		    m_nodes_map[p_orbiter] = orbiter_ptr;
+            orbiter_ptr->name = p_orbiter;
+        }
+
+        orbiter_ptr = m_nodes_map[p_orbiter];
+
+		auto center_node_itr = m_nodes_map.find(p_center);
+
+		if (center_node_itr == m_nodes_map.end()) {
 			node* center_ptr = new node();
 			m_nodes_map[p_center] = center_ptr;
-			m_node_list.push_back(p_center);
+            center_ptr->name = p_center;
+
+            if(p_center == "COM")
+                center_ptr->is_visited = true;
 		}
 
 		orbiter_ptr->ptr_center = m_nodes_map[p_center];
 	}
 
-	uint32_t get_orbit_number() {
+	uint32_t get_orbit_count(){
 
 		uint32_t ret{};
 
-		for (auto& ele : m_nodes_map) {
-			ret++; // direct orbitting
+        _visit_graph();
 
-			auto ptr_next = ele.second;
-			while (ptr_next != nullptr) {
-				ret++; //indirect orbitting
-				ptr_next = ptr_next->ptr_center;
-			}
+
+		for (auto& ele : m_nodes_map) {
+			ret += ele.second->distance;
 		}
 
 		return ret;
 	}
+
 
 	~system_t() {
 		for (auto& ele : m_nodes_map) {
@@ -80,6 +123,8 @@ public:
 	}
 };
 
+
+
 void task_1(std::map<std::string, std::string> p_orbits) {
 	
 	system_t system_obj;
@@ -87,15 +132,13 @@ void task_1(std::map<std::string, std::string> p_orbits) {
 	for (auto& ele : p_orbits) {
 		system_obj.add_orbit(ele.second , ele.first);
 	}
-
-
-	std::cout << "number of orbits :"  << system_obj.get_orbit_number() <<std::endl;
+    
+	std::cout << "number of orbits :"  << system_obj.get_orbit_count() <<std::endl;
 
 }
 
 void task_2(){
-
-
+	
 }
 
 int main() {
