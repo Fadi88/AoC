28da0d15eb5a9af4809a8019d81fcc83c097aa74
Fadi Sedik <fady88@gmail.com>
2019-12-08 07:00:00 +0100

    adding day 8 solution both tasks, that one was easy

--- 2019/day8/day8.cpp
+++ 2019/day8/day8.cpp
@@ -40,28 +40,101 @@ std::vector<std::string> string2vector(std::string input_txt) {
 	return ret;
 }
 
-void task_1() {
+void task_1(std::string m_input) {
+
+    std::vector<std::vector<uint8_t>> image;
+
+    uint16_t current_idx{};
+
+    std::vector<uint8_t> tmp_vector;
+    for (auto tmp_char : m_input) {
+
+        tmp_vector.push_back(static_cast<uint8_t>(tmp_char));
+        current_idx++;
+
+        if (current_idx == (25 * 6)) {
+            current_idx = 0;
+            image.push_back(tmp_vector);
+            tmp_vector.clear();
+        }
+
+    }
+
+    uint16_t min_zeros{std::numeric_limits<uint16_t>::max()};
+
+    std::vector<uint8_t> target_layer;
+    for (auto vec : image) {
+        if (std::count(vec.begin(), vec.end(), '0') < min_zeros) {
+            min_zeros = std::count(vec.begin(), vec.end(), '0');
+            target_layer = vec;
+        }
+    }
+    uint16_t ones, twos;
+    std::cout << "ones in target layer are : " << (ones = std::count(target_layer.begin(), target_layer.end(), '1')) <<std::endl;
+    std::cout << "twos in target layer are : " << (twos = std::count(target_layer.begin(), target_layer.end(), '2')) <<std::endl;
+
+    std::cout << " ones x twos = " << ones * twos << std::endl;
 
 }
 
-void task_2(){
+void task_2(std::string m_input){
+    std::vector<std::vector<uint8_t>> image;
 
+    uint16_t current_idx{};
 
+    std::vector<uint8_t> tmp_vector;
+    for (auto tmp_char : m_input) {
+
+        tmp_vector.push_back(static_cast<uint8_t>(tmp_char));
+        current_idx++;
+
+        if (current_idx == (25 * 6)) {
+            current_idx = 0;
+            image.push_back(tmp_vector);
+            tmp_vector.clear();
+        }
+
+    }
+       
+    std::vector<uint8_t> composite_img(25*6 , '2');
+
+    for (auto layer : image) {
+
+        for (std::size_t idx{}; idx < layer.size(); ++idx) {
+            if (composite_img[idx] == '2')
+                composite_img[idx] = layer[idx];
+        }
+    }
+
+    for (std::size_t idx{}; idx < composite_img.size(); ++idx) {
+        if (idx % 25 == 0)
+            std::cout << std::endl;
+
+        if (composite_img[idx] == '1')
+            std::cout << '*';
+        else
+            std::cout << ' ';
+
+    }
+    std::cout << std::endl<<std::endl;
+    
 }
 
 int main() {
-	std::ifstream input_fd{ "input\\day5_input.txt" };
+	std::ifstream input_fd{ "input\\day8_input.txt" };
 
+    std::string tmp;
+    input_fd >> tmp;
 
 
 	{
 		timer t1("task 1");
-		task_1();
+		task_1(tmp);
 	}
 	
 	{
 		timer t1("task 2");
-		task_2();
+		task_2(tmp);
 	}
 
 	return 0;
