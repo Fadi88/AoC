04f68607db3bdcefe34d4619ac62f3cf312af3ac
root <root@DESKTOP-RJODEIS.localdomain>
2020-11-18 14:06:45 +0100

    removing template file in favor of hard coding generator

--- 2020/template.py
+++
@@ -1,9 +0,0 @@
-import time
-def profiler(method):
-    def wrapper_method(*arg, **kw):
-        t = time.time()
-        method(*arg, **kw)
-        print('Method '  + method.__name__ +' took : ' + "{:2.5f}".format(time.time()-t) + ' sec')
-    return wrapper_method
-
-f_in = open('input.txt', 'r')
