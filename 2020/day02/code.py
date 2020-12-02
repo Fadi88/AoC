8ce5b616c1dde6e1e6b11870950c628db3b373f0
Fadi Sedik <fadi.sedik@gmail.com>
2020-12-02 07:15:29 +0100

    adding day 02 2020 both parts

--- 2020/day02/code.py
+++ 2020/day02/code.py
@@ -1,4 +1,5 @@
 import time,os
+import re
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
@@ -10,20 +11,41 @@ def profiler(method):
 @profiler
 def part1():
 
+    cnt = 0
     with open('input.txt', 'r') as f_in:
-        pass
+        for l in f_in:
+            lim = re.findall(r'\d+' , l)
+            ch = l [l.find(':') - 1]
+            password = l [l.find(':') +1:].strip()
+            if  int(lim[0]) <= password.count(ch)  and password.count(ch) <= int(lim[1]):
+                cnt += 1
+        print("part 1 answer : " ,cnt)
     
 
 @profiler
 def part2():
     with open('input.txt', 'r') as f_in:
-        pass
+        cnt = 0
+    with open('input.txt', 'r') as f_in:
+        for l in f_in:
+            pos = re.findall(r'\d+' , l)
+            ch = l [l.find(':') - 1]
+            password = l [l.find(':') +1:].strip()
+            min = False
+            max = False
+
+            if  int(pos[0])  <= len(password):
+                min = password[int(pos[0]) - 1] == ch
+
+            if  int(pos[1])  <= len(password):
+                max = password[int(pos[1]) - 1] == ch
+
+            if min != max:
+                cnt += 1
+        print("part 2 answer : " ,cnt)
 
 if __name__ == "__main__":
     
 
     part1()
     part2()
-
-    
-    
