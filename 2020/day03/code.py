1964754899e952617b53e7647d2c82d6b177da1b
Fadi Sedik <fadi.sedik@gmail.com>
2020-12-03 06:49:02 +0100

    adding day 3 2018 both parts and day 3 2020 both parts

--- 2020/day03/code.py
+++ 2020/day03/code.py
@@ -1,4 +1,4 @@
-import time,os
+import time,os,re
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
@@ -10,20 +10,51 @@ def profiler(method):
 @profiler
 def part1():
 
+    cnt = 0
+    l_idx = 0
+    dx , dy = (3,1)
     with open('input.txt', 'r') as f_in:
-        pass
-    
+        for l in f_in:
+            if l_idx % len(l.strip())== 0:
+                y = 0
+            else:
+                y = (dx * l_idx) % len(l.strip())
+            if l[y] == '#':
+                cnt += 1
+
+            l_idx += dy
+        print("part 1 : " ,cnt)
+
+
 
 @profiler
 def part2():
-    with open('input.txt', 'r') as f_in:
-        pass
+        
+    
+    trials = [(1,1) , (3,1) , (5,1) , (7,1) , (1,2)]
+    prod = 1
+    for dx, dy in trials:
+        with open('input.txt', 'r') as f_in:
+            cnt = 0
+            l_idx = 0
+            for l in f_in:
+                if l_idx % dy != 0:
+                    l_idx += 1
+                    continue
+
+                if l_idx % len(l.strip())== 0:
+                    y = 0
+                else:
+                    y = (dx * l_idx) % len(l.strip())
+                if l[y] == '#':
+                    cnt += 1
+                l_idx += 1
+
+            print(cnt)
+            prod *= cnt
+    print("part 2 : " ,prod)
 
 if __name__ == "__main__":
-    
 
     part1()
     part2()
-
-    
-    
