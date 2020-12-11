84f2b28a30e6e4c782295b02241fa6e1b79f07c6
Fadi Sedik <fady88@gmail.com>
2020-12-11 07:34:24 +0100

    day 10 2020 both parts

--- 2020/day10/code.py
+++ 2020/day10/code.py
@@ -1,29 +1,57 @@
 import time,os
+from collections import defaultdict
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
         t = time.time()
-        method(*arg, **kw)
+        ret =method(*arg, **kw)
         print('Method '  + method.__name__ +' took : ' + "{:2.5f}".format(time.time()-t) + ' sec')
+        return ret
     return wrapper_method
 
 @profiler
 def part1():
 
-    with open('input.txt', 'r') as f_in:
-        pass
+    l =  [int(tmp.strip()) for tmp in open('input.txt', 'r').readlines()]
+
+    l.append(0)
+    l.append(max(l) + 3)
+    l.sort()
+
+
+    d1 = 0 
+    d3 = 0
+
+    for i in range(1, len(l)):
+        if l[i] - l[i - 1 ] == 1:
+            d1 +=1
+        elif l[i] - l[i - 1 ] == 3 :
+            d3 += 1
+        else :
+            print(l[i] - l[i - 1 ])
+
+    print('part 1 answer : ' , d1*d3)
     
 
 @profiler
 def part2():
-    with open('input.txt', 'r') as f_in:
-        pass
+
+    l =  [int(tmp.strip()) for tmp in open('input.txt', 'r').readlines()]
+
+    l.append(0)
+    l.sort()
+
+    comb = defaultdict(lambda : 0)
+
+    comb[l[-1]+3] = 1
+    
+    for v in l[::-1]:
+        comb[v] = comb[v+1] + comb[v+2] + comb[v+3]
+        #print(v , comb[v])
+
+    print("Part 2 answer :", comb[0])
 
 if __name__ == "__main__":
-    
 
     part1()
     part2()
-
-    
-    
