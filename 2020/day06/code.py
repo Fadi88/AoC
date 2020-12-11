0dc5f58ab7be396ba3b10e32f76103e4e62a431b
Fadi Sedik <fady88@gmail.com>
2020-12-11 07:33:42 +0100

    day 07 2020

--- 2020/day06/code.py
+++ 2020/day06/code.py
@@ -1,4 +1,5 @@
 import time,os
+from functools import reduce
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
@@ -11,13 +12,8 @@ def profiler(method):
 def part1():
 
     with open('input.txt', 'r') as f_in:
-        ls = [set(t.replace('\n' , '')) for t in f_in.read().split('\n\n')]
-        
-        cnt = 0
-        for gr in ls :
-            cnt += len(gr)
-        print('part 1 answer : ' , cnt)
-    
+        print('part 1 answer : ' , sum(map(len, [set(t.replace('\n' , '')) for t in f_in.read().split('\n\n')])))
+
 
 @profiler
 def part2():
@@ -29,17 +25,13 @@ def part2():
             indvs = gr.split('\n')
             tmp = set(indvs[0])
 
-            for indv in indvs :
-                tmp = tmp.intersection(indv)
+            for indv in indvs[1:] :
+                tmp &= set(indv)
             cnt += len(tmp)
             
         print('part 2 answer : ' , cnt)
 
 if __name__ == "__main__":
-    
 
     part1()
     part2()
-
-    
-    
--- 2020/day07/code.py
+++ 2020/day07/code.py
@@ -1,29 +1,63 @@
-import time,os
+import time,os,re
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
         t = time.time()
-        method(*arg, **kw)
+        ret = method(*arg, **kw)
         print('Method '  + method.__name__ +' took : ' + "{:2.5f}".format(time.time()-t) + ' sec')
+        return ret
     return wrapper_method
+
+target_color = 'shiny gold'
+
+bags = {}
+bags_count = {}
+
+def contains_target(color):
+    if color is None or bags[color] is None:
+        return False
+    elif target_color in bags[color] :
+        return True
+    else:
+        return any(map(contains_target , bags[color]))
 
 @profiler
 def part1():
 
     with open('input.txt', 'r') as f_in:
-        pass
-    
+        for l in f_in:
+            color = re.search(r'^\w+ \w+' , l)
+            if 'no other bags' in l:
+                bags[color.group(0)] = None
+            else:
+                bags[color.group(0)] =re.findall(r'\d+ (\w+ \w+)' , l)
+                bags_count[color.group(0)] = list(map(int,re.findall(r'(\d+)' , l)))
+
+        cnt = 0
+        for bag_color in bags:
+            if contains_target(bag_color):
+                cnt += 1
+
+        print('part 1 answer : ' , cnt)
+
+def get_content_count(color):
+    cnt = 0
+
+    for cr_cnt , cr_color in zip(bags_count[color] , bags[color]):
+
+        if cr_color in bags and bags[cr_color] is not None:
+            cnt += (cr_cnt * get_content_count(cr_color))
+        else :
+            cnt += cr_cnt 
+
+    return cnt
+
 
 @profiler
 def part2():
-    with open('input.txt', 'r') as f_in:
-        pass
+    print('part 2 answer : ' , get_content_count(target_color))
 
 if __name__ == "__main__":
-    
 
     part1()
     part2()
-
-    
-    
