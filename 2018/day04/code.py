a7129ad75dec21a84f85f9e6329629cabc1852b6
Fadi Sedik <fady88@gmail.com>
2020-12-11 07:34:04 +0100

    2018 day 4 part 1

--- 2018/day04/code.py
+++ 2018/day04/code.py
@@ -1,4 +1,5 @@
-import time,os
+import time,os,re
+from collections import defaultdict
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
@@ -10,13 +11,41 @@ def profiler(method):
 @profiler
 def part1():
     ls = []
+    tracker = defaultdict(list)
     with open('input.txt', 'r') as f_in:
         for l in f_in:
             ls.append(l.strip())
 
-
+        ls.sort()
         for l in ls:
-            print(l)
+            if '#' in l:
+                guard = int(re.findall(r'#(\d+)' , l)[0])
+            elif 'falls asleep' in l:
+                beg = int(re.findall(r':(\d+)' , l)[0])
+            elif 'wakes up' in l:
+                end = int(re.findall(r':(\d+)' , l)[0])
+                tracker[guard].append((beg , end))
+        
+    total_sleep = {}
+    for guard in tracker:
+        sum = 0
+        for nap in tracker[guard]:
+            sum += nap[1] - nap[0]
+        total_sleep[guard] = sum
+
+    max_sleep = max(total_sleep.values())
+    chosen_1 = list(filter(lambda x:x[1] == max_sleep,total_sleep.items()))[0][0]
+    
+    hist = [0] * 60
+
+    for nap in tracker[chosen_1]:
+        for i in range(nap[0] , nap[1]):
+            hist[i] += 1
+        
+    hi_pr = hist.index(max(hist))
+
+    print(hi_pr * chosen_1)
+
     
 
 @profiler
--- 2020/day08/code.py
+++ 2020/day08/code.py
@@ -1,5 +1,4 @@
 import time,os
-from collections import defaultdict
 
 class computer:
     def __init__(self):
@@ -9,28 +8,28 @@ class computer:
         self.term = False
 
     def set_code(self , in_code):
-        self.code =in_code
+        self.code = in_code
         self.acc  = 0
         self.pc   = 0
 
     def exec(self):
-        int_tracker = defaultdict(lambda : False)
+        int_tracker = set()
 
-        while self.pc != len(self.code) and not int_tracker[self.pc] == True:
+        while self.pc != len(self.code) and not self.pc in int_tracker:
             
             cur = self.code[self.pc]
 
             if cur[0] == 'nop':
-                int_tracker[self.pc] = True
+                int_tracker.add(self.pc)
                 self.pc += 1
 
             elif cur[0] == 'acc':
-                int_tracker[self.pc] = True
+                int_tracker.add(self.pc)
                 self.acc += cur[1]
                 self.pc += 1
 
             elif cur[0] == 'jmp':
-                int_tracker[self.pc] = True
+                int_tracker.add(self.pc)
                 self.pc += cur[1]
             
             else :
@@ -39,6 +38,8 @@ class computer:
         if self.pc == len(self.code):
             self.term = True
 
+    def get_result(self):
+        return self.acc
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
@@ -50,7 +51,7 @@ def profiler(method):
 @profiler
 def part1():
 
-    inst = list()
+    inst = []
     comp_obj = computer()
     for l in open('input.txt', 'r').readlines():
         tmp = l.strip().split()
@@ -58,17 +59,17 @@ def part1():
 
     comp_obj.set_code(inst)
     comp_obj.exec()
-    print('part 1 answer : ' , comp_obj.acc)
+    print('part 1 answer : ' , comp_obj.get_result())
 
 @profiler
 def part2():
-    inst = list()
+    inst = []
     comp_obj = computer()
     for l in open('input.txt', 'r').readlines():
         tmp = l.strip().split()
         inst.append((tmp[0] , int(tmp[1])))
 
-
+ 
     for l in range(len(inst)):
         if inst[l][0] == 'nop' or  inst[l][0] == 'jmp':
             mod = inst.copy()
@@ -77,7 +78,7 @@ def part2():
             comp_obj.exec()
 
             if comp_obj.term == True:
-                print('part 2 answer : ' , comp_obj.acc)
+                print('part 2 answer : ' , comp_obj.get_result())
                 break
 
 
