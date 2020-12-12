12f62a55358764c2143980306e121af9152236eb
Fadi Sedik <fady88@gmail.com>
2020-12-12 09:05:12 +0100

    day 12 2020 adding different methods for part 2 as refrence

--- 2020/day12/code.py
+++ 2020/day12/code.py
@@ -1,5 +1,6 @@
 import time,os
 import math
+import numpy as np
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
@@ -77,19 +78,96 @@ def part2():
             nx =  wx * math.cos(math.radians(mag)) + wy * math.sin(math.radians(mag))
             ny =  -wx * math.sin(math.radians(mag)) + wy * math.cos(math.radians(mag))
 
-            wx = int(round(nx))
-            wy = int(round(ny))
+            wx = round(nx)
+            wy = round(ny)
  
         elif dr == 'L':
             nx =  wx * math.cos(-math.radians(mag)) + wy * math.sin(-math.radians(mag))
             ny =  -wx * math.sin(-math.radians(mag)) + wy * math.cos(-math.radians(mag))
 
-            wx = int(round(nx))
-            wy = int(round(ny))
+            wx = round(nx)
+            wy = round(ny)
             
     print('part 2 answer : ' , abs(x) +  abs(y))
+
+@profiler
+def p2_matrix():
+
+    pos = np.array([0,0])
+
+    wp  = np.array([10,1]) 
+
+    for l in open('input.txt'):
+        dr = l[0]
+        mag = int(l.strip()[1:])
+
+        if dr == 'F' :
+            pos += mag * wp
+
+        elif dr == 'N':
+            wp += mag * np.array([0,1])
+
+        elif dr == 'S':
+            wp += mag * np.array([0,-1])
+
+        elif dr == 'E':
+            wp += mag * np.array([1,0])
+
+        elif dr == 'W':
+            wp += mag * np.array([-1,0])
+
+        elif dr == 'R':
+            theta = np.radians(mag)
+            Rt = np.array([[np.cos(theta) , -np.sin(theta)] , [np.sin(theta) , np.cos(theta)]])
+            wp = np.rint(wp.dot(Rt)).astype(int)
+ 
+        elif dr == 'L':
+            theta = np.radians(-mag)
+            Rt = np.array([[np.cos(theta) , -np.sin(theta)] , [np.sin(theta) , np.cos(theta)]])
+
+            wp = np.rint(wp.dot(Rt)).astype(int)
+            
+    print('part 2 using matrix answer : ' , np.abs(pos[0]) +  np.abs(pos[1]))
+
+
+@profiler
+def p2_complex():
+
+    pos = 0 + 0j
+
+    wp  = 10 + 1j
+
+    for l in open('input.txt'):
+        dr = l[0]
+        mag = int(l.strip()[1:])
+
+        if dr == 'F' :
+            pos += mag * wp
+
+        elif dr == 'N':
+            wp += mag * 1j
+
+        elif dr == 'S':
+            wp -= mag * 1j
+
+        elif dr == 'E':
+            wp += mag * 1
+
+        elif dr == 'W':
+            wp -= mag * 1
+
+        elif dr == 'R':
+           wp *= 1j**(-mag/90)
+ 
+        elif dr == 'L':
+           wp *= 1j**(mag/90)
+            
+    print('part 2 using complex answer : ' , int(abs(pos.real) +  abs(pos.imag)))
 
 if __name__ == "__main__":
 
     part1()
     part2()
+    p2_complex()
+    p2_matrix()
+    
