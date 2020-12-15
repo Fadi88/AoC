4086490ecbcecca2b1682db2c6c8b60d7a10fe5e
Fadi Sedik <fady88@gmail.com>
2020-12-15 07:01:41 +0100

    day 15 2020

--- 2020/day15/code.py
+++ 2020/day15/code.py
@@ -1,29 +1,53 @@
 import time,os
+from collections import defaultdict
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
         t = time.time()
-        method(*arg, **kw)
+        ret = method(*arg, **kw)
         print('Method '  + method.__name__ +' took : ' + "{:2.5f}".format(time.time()-t) + ' sec')
+        return ret
     return wrapper_method
 
 @profiler
 def part1():
 
-    with open('input.txt', 'r') as f_in:
-        pass
-    
+    input = list(map(int, open('input.txt', 'r').read().split(',')))
+
+    for _ in range(2020):
+        if input.count(input[-1]) == 1 :
+            input.append(0)
+        else :
+          idx = [i for i,num in enumerate(input) if num == input[-1]]
+          input.append(idx[-1] - idx[-2])
+        
+    print(input[2019])
+        
 
 @profiler
 def part2():
-    with open('input.txt', 'r') as f_in:
-        pass
+
+    input = list(map(int, open('input.txt', 'r').read().split(',')))
+
+    age = defaultdict(list)
+
+    for i in range(30000000):
+        if len(input) == 0 :
+            if len(age[mem]) < 2 :
+                next = 0
+            else :
+                next = age[mem][-1] - age[mem][-2]
+            age[next].append(i)
+            mem = next
+
+        else :
+            mem = input.pop(0)
+            age[mem].append(i)
+            
+
+    print(mem)
 
 if __name__ == "__main__":
-    
 
     part1()
     part2()
-
-    
-    
