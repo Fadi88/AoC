e647beda3125c0b272f8e2eb8444c7b31d0bf4c3
Fadi Sedik <fady88@gmail.com>
2020-12-16 20:15:00 +0100

    refactor day 16 2020

--- 2020/day16/code.py
+++ 2020/day16/code.py
@@ -1,17 +1,12 @@
-import time,os
-import re
+import time,os,re,operator
+from functools import reduce
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
         t = time.time()
-        ret = method(*arg, **kw)
+        method(*arg, **kw)
         print('Method '  + method.__name__ +' took : ' + "{:2.5f}".format(time.time()-t) + ' sec')
-        return ret
     return wrapper_method
-
-
-def check_valid_rule(val,rule):
-    return not( rule[0][1] <= val <= rule[0][1] or rule[0][1] <= val <= rule[0][1])
 
 @profiler
 def part1():
@@ -22,25 +17,17 @@ def part1():
 
     for l in open('input.txt', 'r').read().split('\n'):
         if 'or' in l:
-            vals = [*map(int, re.findall(r'(\d+)' , l))]
+            vals = list(map(int, re.findall(r'(\d+)' , l)))
             rules.append(((vals[0] , vals[1]) , (vals[2] , vals[3])))
             comb_rules.update(set(range(vals[0] , vals[1] + 1)))
             comb_rules.update(set(range(vals[2] , vals[3] + 1)))
 
-        elif re.match(r'^(\d+,){19}\d+$' , l ):
-            vals = [*map(int, re.findall(r'(\d+)' , l))]
-            if len(my_ticket) != 0:
-                other_tickets.append(vals)
-            else:
-                my_ticket = vals
+        elif ',' in l:
+            vals = list(map(int , l.split(',')))
+            if len(my_ticket) != 0 : other_tickets.append(vals)
+            else : my_ticket = vals
 
-    cnt = 0
-    for tik in other_tickets:
-        for val in tik:
-            if val not in comb_rules:
-                cnt += val
-
-    print(cnt)
+    print(sum([val for tik in other_tickets for val in tik if val not in comb_rules]))
 
 @profiler
 def part2():
@@ -51,38 +38,26 @@ def part2():
 
     for l in open('input.txt', 'r').read().split('\n'):
         if 'or' in l:
-            vals = [*map(int, re.findall(r'(\d+)' , l))]
+            vals = list(map(int, re.findall(r'(\d+)' , l)))
             comb_rules.update(set(range(vals[0] , vals[1] + 1)))
             comb_rules.update(set(range(vals[2] , vals[3] + 1)))
             rule_map[l[:l.find(':') ]] = set(range(vals[0] , vals[1] + 1)) | set(range(vals[2] , vals[3] + 1))
 
-        elif re.match(r'^(\d+,){19}\d+$' , l ):
-            vals = [*map(int, re.findall(r'(\d+)' , l))]
-            if len(my_ticket) != 0:
-                other_tickets.append(vals)
-            else:
-                my_ticket = vals
+        elif ',' in l:
+            vals = list(map(int , l.split(',')))
+            if len(my_ticket) != 0 : other_tickets.append(vals)
+            else : my_ticket = vals
 
-    pos = {}
-    for i in range(len(my_ticket)):
-        pos[i] = set(rule_map.keys())
+    pos = { i : set(rule_map.keys()) for i in range(len(my_ticket)) }
 
     for tik in other_tickets:
-        valid_ticket = True
-        for val in tik:
-            if val not in comb_rules:
-                valid_ticket = False
-                break
+        if any([val not in comb_rules for val in tik]) : continue
 
-        if not valid_ticket:
-            continue
-        
         for idx,val in enumerate(tik):
             for rule in rule_map:
-                if val not in rule_map[rule]:
-                    pos[idx].remove(rule)
+                if val not in rule_map[rule] : pos[idx].remove(rule)
 
-    visited = [False] * len(pos)
+    visited = [False for _ in range(len(pos))]
 
     for _ in range(len(pos)):
         target_idx = -1
@@ -95,21 +70,10 @@ def part2():
                 break
         
         for idx in pos:
-            if idx == target_idx:
-                continue
-            if target_field in pos[idx]:
-                pos[idx].remove(target_field)
+            if idx == target_idx : continue
+            if target_field in pos[idx] : pos[idx].remove(target_field)
 
-    departure_idx = []
-    for ele in pos:
-        if 'departure' in pos[ele].pop():
-            departure_idx.append(ele)
-
-    prod = 1
-    for idx in departure_idx:
-        prod *= my_ticket[idx]
-
-    print(prod)
+    print(reduce(operator.mul , [ my_ticket[i] for i in pos if 'departure' in pos[i].pop()]))
 
 if __name__ == "__main__":
 
