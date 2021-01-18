e3892aab7fc05ae2ba8f38f2c5ab3697fd3d085c
Fadi Sedik <fady88@gmail.com>
2021-01-18 19:15:38 +0100

    code clean up

--- 2017/day04/code.py
+++ 2017/day04/code.py
@@ -14,21 +14,6 @@ def profiler(method):
 @profiler
 def part1():
 
-    import time
-
-
-def profiler(method):
-    def wrapper_method(*arg, **kw):
-        t = time.time()
-        method(*arg, **kw)
-        print('Method ' + method.__name__ + ' took : ' +
-              "{:2.5f}".format(time.time()-t) + ' sec')
-    return wrapper_method
-
-
-@profiler
-def part1():
-
     cnt = 0
     for l in open('input.txt'):
         p_list = l.strip().split()
