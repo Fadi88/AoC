de4385d47209730af7dbb85233aa023408660b4e
Fadi Sedik <fady88@gmail.com>
2021-12-03 06:59:41 +0100

    adding day03 2021 both parts in python

--- 2021/day03/code.py
+++ 2021/day03/code.py
@@ -1,5 +1,5 @@
 import time
-import re
+from collections import defaultdict
 
 
 def profiler(method):
@@ -14,12 +14,55 @@ def profiler(method):
 
 @profiler
 def part1():
-    pass
+    nums = [l.strip() for l in open("input.txt")]
+
+    gamma = list(nums[0])
+    epsilon = list(nums[0])
+
+    for bit in range(len(nums[0])):
+        bits = [l[bit] for l in nums]
+
+        if bits.count('1') > bits.count('0'):
+            gamma[bit] = '1'
+            epsilon[bit] = '0'
+        else:
+            gamma[bit] = '0'
+            epsilon[bit] = '1'
+
+    print("part 1 : ", int(''.join(gamma), 2) * int(''.join(epsilon), 2))
 
 
 @profiler
 def part2():
-    pass
+
+    nums_oxy = [l.strip() for l in open("input.txt")]
+    nums_car = nums_oxy.copy()
+
+    for bit in range(len(nums_oxy[0])):
+
+        bits_oxy = [l[bit] for l in nums_oxy]
+
+        if bits_oxy.count('1') >= bits_oxy.count('0'):
+            nums_oxy = [l for l in nums_oxy if l[bit] == '1']
+        else:
+            nums_oxy = [l for l in nums_oxy if l[bit] == '0']
+
+        if len(nums_oxy) == 1:
+            break
+
+    for bit in range(len(nums_car[0])):
+
+        bits_car = [l[bit] for l in nums_car]
+
+        if bits_car.count('1') >= bits_car.count('0'):
+            nums_car = [l for l in nums_car if l[bit] == '0']
+        else:
+            nums_car = [l for l in nums_car if l[bit] == '1']
+
+        if len(nums_car) == 1:
+            break
+
+    print("part 2 : ", int(nums_oxy[0], 2) * int(nums_car[0], 2))
 
 
 if __name__ == "__main__":
