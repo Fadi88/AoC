b496698154222f9c3281f1ec9cc1c9a4e1c9f658
Fadi Sedik <fady88@gmail.com>
2021-12-06 09:45:05 +0100

    adding day06 2021 rust

--- 2021/day06/main.rs
+++ 2021/day06/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::time;
 
@@ -10,10 +11,63 @@ fn bench(f: fn()) {
 }
 
 fn part_1() {
-    for _l in fs::read_to_string("day06/input.txt").unwrap().lines() {}
+    let mut input: Vec<_> = fs::read_to_string("day06/input.txt")
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<u8>().unwrap())
+        .collect();
+
+    for _ in 0..80 {
+        let mut tmp: Vec<u8> = Vec::new();
+        for i in 0..input.len() {
+            if input[i] == 0 {
+                tmp.push(6);
+                tmp.push(8);
+            } else {
+                tmp.push(input[i] - 1);
+            }
+        }
+        input = tmp;
+    }
+    println!("{:?}", input.len())
 }
 
-fn part_2() {}
+fn part_2() {
+    let input: Vec<u8> = fs::read_to_string("day06/input.txt")
+        .unwrap()
+        .split(',')
+        .map(|x| x.parse::<u8>().unwrap())
+        .collect();
+
+    let mut tracker: HashMap<u8, u64> = HashMap::new();
+
+    for i in input {
+        *tracker.entry(i).or_insert(0) += 1;
+    }
+
+    for _ in 0..256 {
+        let mut tmp: HashMap<u8, u64> = HashMap::new();
+        for i in tracker.clone().keys() {
+            let x = *tracker.get(i).unwrap();
+            if *i == 0 {
+                *tmp.entry(6).or_insert(0) += x;
+                *tmp.entry(8).or_insert(0) = x;
+            } else {
+                *tmp.entry(i - 1).or_insert(0) += x;
+            }
+        }
+        tracker = tmp.clone();
+    }
+
+    println!(
+        "{:?}",
+        tracker
+            .into_values()
+            .collect::<Vec<u64>>()
+            .iter()
+            .sum::<u64>()
+    );
+}
 
 fn main() {
     bench(part_1);
