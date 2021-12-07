4c67186a17ab77e9f2e4d6466746832caa19a2d4
Fadi Sedik <fady88@gmail.com>
2021-12-07 09:53:05 +0100

    refactor rust day 07 2021

--- 2021/day07/main.rs
+++ 2021/day07/main.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use std::time;
 
 fn bench(f: fn()) {
@@ -9,61 +8,54 @@ fn bench(f: fn()) {
     ret
 }
 
-fn get_fuel_p1(input: &Vec<i16>, i: i16) -> u32 {
-    let res = input
-        .iter()
-        .map(|x| (x - i).abs() as u32)
-        .collect::<Vec<_>>();
+fn get_fuel_p1(input: &[i32], i: i32) -> i32 {
+    let res = input.iter().map(|x| (x - i).abs()).collect::<Vec<_>>();
 
     res.iter().sum()
 }
 
 fn part_1() {
-    let input = fs::read_to_string("day07/input.txt")
-        .unwrap()
+    let input = include_str!("input.txt")
         .split(',')
-        .map(|x| x.parse::<i16>().unwrap())
+        .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
 
-    let p1 = input
+    let p1 = *input
         .iter()
         .map(|v| get_fuel_p1(&input, v.clone()))
         .collect::<Vec<_>>()
         .iter()
         .min()
-        .unwrap()
-        .clone();
+        .unwrap();
 
     println!("part 1 : {:?}", p1);
 }
 
-fn get_fuel_p2(input: &Vec<u32>, i: u32) -> u32 {
+fn get_fuel_p2(input: &[i32], i: i32) -> i32 {
     let res = input
         .iter()
         .map(|x| {
-            let dist = ((x - i) as i32).abs();
-            (dist * (dist + 1) / 2) as u32
+            let dist = (x - i).abs();
+            dist * (dist + 1) / 2
         })
-        .collect::<Vec<u32>>();
+        .collect::<Vec<_>>();
 
     res.iter().sum()
 }
 
 fn part_2() {
-    let input = fs::read_to_string("day07/input.txt")
-        .unwrap()
+    let input = include_str!("input.txt")
         .split(',')
-        .map(|x| x.parse::<u32>().unwrap())
+        .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
 
-    let p2 = input
+    let p2 = *input
         .iter()
         .map(|v| get_fuel_p2(&input, v.clone()))
         .collect::<Vec<_>>()
         .iter()
         .min()
-        .unwrap()
-        .clone();
+        .unwrap();
 
     println!("part 2 : {:?}", p2);
 }
