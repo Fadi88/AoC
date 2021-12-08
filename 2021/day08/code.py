71706fec4b1386e06f2eaa358ad9061c424f002c
Fadi Sedik <fady88@gmail.com>
2021-12-08 13:31:04 +0100

    improvement day 8 2021 python

--- 2021/day08/code.py
+++ 2021/day08/code.py
@@ -13,20 +13,11 @@ def profiler(method):
 
 @profiler
 def part1():
-    # 2 : 1
-    # 3 : 7
-    # 4 : 4,
-    # 5 : 2,3,5,
-    # 6 : 0,6,9
-    # 7 : 8
-
     cnt = 0
     for l in open("day08/input.txt"):
-        l = l.split(' | ')
-        patterns = l[1].split()
-        for pattern in patterns:
-            if len(pattern) in [2, 4, 3, 7]:
-                cnt += 1
+        patterns = l.split(' | ')[1].split()
+
+        cnt += sum([len(pattern) in [2, 4, 3, 7] for pattern in patterns])
 
     print("part 1 : ", cnt)
 
@@ -61,22 +52,19 @@ def part2():
                 mapping[8] = set(pattern)
 
         for pattern in patterns:
-            if len(pattern) == 6:  # 0,6,9
-                if len(mapping[1] - set(pattern)) == 1:  # 6
-                    mapping[6] = set(pattern)
-
-        for pattern in patterns:
             if len(pattern) == 5:  # 2,3,5
                 if len(mapping[1] - set(pattern)) == 0:  # 3
                     mapping[3] = set(pattern)
-                elif len(set(pattern) - mapping[6]) == 0:  # 5
+                elif len(set(pattern) - mapping[4]) == 2:  # 5
                     mapping[5] = set(pattern)
-                elif len(set(pattern) - mapping[6]) == 1:  # 2
+                elif len(set(pattern) - mapping[4]) == 3:  # 2
                     mapping[2] = set(pattern)
 
         for pattern in patterns:
-            if len(pattern) == 6 and set(pattern) not in mapping:  # 0,9
-                if len(mapping[5] - set(pattern)) == 0:
+            if len(pattern) == 6:  # 0,6,9
+                if len(mapping[1] - set(pattern)) == 1:  # 6
+                    mapping[6] = set(pattern)
+                elif len(mapping[5] - set(pattern)) == 0:
                     mapping[9] = set(pattern)
                 else:
                     mapping[0] = set(pattern)
@@ -84,11 +72,11 @@ def part2():
         s = ""
         for p in l[1].split(" "):
             s += str(mapping.index(set(p.strip())))
-        
+
         total += int(s)
 
-    print(total)
-            
+    print("part 2 : ", total)
+
 
 if __name__ == "__main__":
 
