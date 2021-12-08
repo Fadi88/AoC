6e5456d7b4990e8f02c466685e729539e9e117d6
Fadi Sedik <fady88@gmail.com>
2021-12-08 18:35:19 +0100

    adding other methof python for part 2 day 8 2021

--- 2021/day08/code.py
+++ 2021/day08/code.py
@@ -1,4 +1,5 @@
 import time
+from collections import Counter, defaultdict
 
 
 def profiler(method):
@@ -77,7 +78,51 @@ def part2():
     print("part 2 : ", total)
 
 
+@profiler
+def part2_segment():
+    t = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg',
+         'acf', 'abcdefg', 'abcdfg']
+
+    cnt_ref = Counter(''.join(t))
+    ref = defaultdict(list)
+
+    for i in cnt_ref:
+        ref[cnt_ref[i]].append(i)
+
+    total = 0
+    for l in open('day08/input.txt'):
+
+        p = l.strip().split(' | ')
+
+        c = Counter(''.join(p[0].split(' ')))
+
+        mapping = {}
+
+        four = list(filter(lambda x: len(x) == 4, p[0].split(' ')))[0]
+        for i in c:
+            if c[i] in [4, 6, 9]:
+                mapping[i] = ref[c[i]][0]
+            elif c[i] == 7:
+                mapping[i] = 'd' if i in four else 'g'
+            elif c[i] == 8:
+                mapping[i] = 'c' if i in four else 'a'
+
+        val = '' 
+        for pat in p[1].split(' '):
+            v = [mapping[ch] for ch in pat]
+            v.sort()
+            v = ''.join(v)
+            
+            val += str(t.index(v))
+
+        total += int(val)
+
+    print(total)
+        
+
+
 if __name__ == "__main__":
 
     part1()
     part2()
+    part2_segment()
