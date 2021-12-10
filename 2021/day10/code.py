6ce14f0dbfd90cdba499a80dc73ad4251cbc1eef
Fadi Sedik <fady88@gmail.com>
2021-12-10 07:29:40 +0100

    adding day10 2021 in python

--- 2021/day10/code.py
+++ 2021/day10/code.py
@@ -13,12 +13,52 @@ def profiler(method):
 
 @profiler
 def part1():
-    pass
+    total = 0
+    
+    illegal = {')' : 3 , ']' : 57 , '}' : 1197 , '>' : 25137}
+
+    for o in open("day10/input.txt"):
+        l = o.strip()
+        while l.count('{}') > 0 or l.count('[]') > 0 or l.count("<>") > 0 or l.count("()") > 0:
+            l = l.replace("{}" , "").replace("<>","").replace("[]","").replace("()","")
+    
+
+        p = [l.index(i) for i in illegal if i in l]
+
+        if len(p) > 0:
+            total += illegal[l[min(p)]]
+
+    print(total)
 
 
 @profiler
 def part2():
-    pass
+    
+    prize = {')' : 1 , ']' : 2 , '}' : 3 , '>' : 4}
+    comp = {'{' : '}' , '(' : ')' , '[' : ']' , '<' : '>'}
+
+    scores = []
+    for o in open("day10/input.txt"):
+        l = o.strip()
+        while l.count('{}') > 0 or l.count('[]') > 0 or l.count("<>") > 0 or l.count("()") > 0:
+            l = l.replace("{}" , "").replace("<>","").replace("[]","").replace("()","")
+    
+
+        p = [l.index(i) for i in prize if i in l]
+
+        if len(p) == 0:
+            local_tmp = 0
+            for i in l[::-1]:
+                local_tmp *= 5
+                local_tmp += prize[comp[i]]
+            print(o.strip() ,l , local_tmp)
+            scores.append(local_tmp)
+
+
+    scores.sort()
+    print(scores)
+    print(scores[len(scores) //2])   
+            
 
 
 if __name__ == "__main__":
