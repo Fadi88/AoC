3cce24005cfc482f7f59b03e27625400e4de10fd
Fadi Sedik <fady88@gmail.com>
2021-12-11 08:04:42 +0100

    adding day 11 2021 python + template rust

---
+++ 2021/day11/code.py
@@ -0,0 +1,97 @@
+import time
+from collections import deque
+
+
+def profiler(method):
+    def wrapper_method(*arg, **kw):
+        t = time.time()
+        ret = method(*arg, **kw)
+        print('Method ' + method.__name__ + ' took : ' +
+              "{:2.5f}".format(time.time()-t) + ' sec')
+        return ret
+    return wrapper_method
+
+
+deltas = [(0, 1), (0, -1), (1, 0), (1, 1), (1, -1), (-1, 0), (-1, 1), (-1, -1)]
+
+
+def print_grid(grid):
+    for l in grid:
+        print(' '.join(map(str, l)))
+
+
+@profiler
+def part1():
+    grid = [list(map(int, list(l.strip()))) for l in open("day11/input.txt")]
+
+    flashes = 0
+    ready = deque()
+
+    for _ in range(100):
+        trig = set()
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                grid[x][y] += 1
+                if grid[x][y] > 9:
+                    ready.append((x, y))
+                    trig.add((x, y))
+
+        while ready:
+            nx, ny = ready.popleft()
+            for dx, dy in deltas:
+                if 0 <= nx+dx < len(grid) and 0 <= ny+dy < len(grid[0]):
+                    grid[nx+dx][ny+dy] += 1
+                    if grid[nx+dx][ny+dy] > 9 and (nx+dx, ny+dy) not in trig:
+                        ready.append((nx+dx, ny+dy))
+                        trig.add((nx+dx, ny+dy))
+
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] > 9:
+                    grid[x][y] = 0
+                    flashes += 1
+
+    print(flashes)
+
+
+@profiler
+def part2():
+
+    grid = [list(map(int, list(l.strip()))) for l in open("day11/input.txt")]
+
+    ready = deque()
+
+    cnt = 0
+    while True:
+        if all([all([i == 0 for i in l]) for l in grid]):
+            break
+        cnt += 1
+        trig = set()
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                grid[x][y] += 1
+                if grid[x][y] > 9:
+                    ready.append((x, y))
+                    trig.add((x, y))
+
+        while ready:
+            nx, ny = ready.popleft()
+            for dx, dy in deltas:
+                if 0 <= nx+dx < len(grid) and 0 <= ny+dy < len(grid[0]):
+                    grid[nx+dx][ny+dy] += 1
+                    if grid[nx+dx][ny+dy] > 9 and (nx+dx, ny+dy) not in trig:
+                        ready.append((nx+dx, ny+dy))
+                        trig.add((nx+dx, ny+dy))
+
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] > 9:
+                    grid[x][y] = 0
+
+    print(cnt)
+
+
+if __name__ == "__main__":
+
+    part1()
+    part2()
---
+++ 2021/day11/main.rs
@@ -0,0 +1,21 @@
+use std::fs;
+use std::time;
+
+fn bench(f: fn()) {
+    let t0 = time::Instant::now();
+    let ret = f();
+    println!("time used {:?}", time::Instant::now().duration_since(t0));
+
+    ret
+}
+
+fn part_1() {
+    for _l in fs::read_to_string("day11/input.txt").unwrap().lines() {}
+}
+
+fn part_2() {}
+
+fn main() {
+    bench(part_1);
+    bench(part_2);
+}
