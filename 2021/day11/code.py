bdc6ad8d759a4584fc5fd2e37c2319af52d69c4d
Fadi Sedik <fady88@gmail.com>
2021-12-11 09:16:53 +0100

    refactor day 11 2021 python

--- 2021/day11/code.py
+++ 2021/day11/code.py
@@ -1,3 +1,4 @@
+import collections
 import time
 from collections import deque
 
@@ -12,12 +13,45 @@ def profiler(method):
     return wrapper_method
 
 
-deltas = [(0, 1), (0, -1), (1, 0), (1, 1), (1, -1), (-1, 0), (-1, 1), (-1, -1)]
+def get_deltas():
+    return [(0, 1), (0, -1), (1, 0), (1, 1), (1, -1), (-1, 0), (-1, 1), (-1, -1)]
 
 
 def print_grid(grid):
     for l in grid:
         print(' '.join(map(str, l)))
+    print()
+
+
+def cycle(grid):
+    flashes = 0
+
+    trig = set()
+    ready = deque()
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            grid[x][y] += 1
+            if grid[x][y] > 9:
+                ready.append((x, y))
+                trig.add((x, y))
+
+    while ready:
+        nx, ny = ready.popleft()
+        for dx, dy in get_deltas():
+            if 0 <= nx+dx < len(grid) and 0 <= ny+dy < len(grid[0]):
+                grid[nx+dx][ny+dy] += 1
+                if grid[nx+dx][ny+dy] > 9 and (nx+dx, ny+dy) not in trig:
+                    ready.append((nx+dx, ny+dy))
+                    trig.add((nx+dx, ny+dy))
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] > 9:
+                grid[x][y] = 0
+                flashes += 1
+
+    return flashes
 
 
 @profiler
@@ -26,34 +60,8 @@ def part1():
 
     flashes = 0
 
-    trig = set()
-    ready = deque()
-
     for _ in range(100):
-
-        trig.clear()
-
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                grid[x][y] += 1
-                if grid[x][y] > 9:
-                    ready.append((x, y))
-                    trig.add((x, y))
-
-        while ready:
-            nx, ny = ready.popleft()
-            for dx, dy in deltas:
-                if 0 <= nx+dx < len(grid) and 0 <= ny+dy < len(grid[0]):
-                    grid[nx+dx][ny+dy] += 1
-                    if grid[nx+dx][ny+dy] > 9 and (nx+dx, ny+dy) not in trig:
-                        ready.append((nx+dx, ny+dy))
-                        trig.add((nx+dx, ny+dy))
-
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] > 9:
-                    grid[x][y] = 0
-                    flashes += 1
+        flashes += cycle(grid)
 
     print(flashes)
 
@@ -69,29 +77,8 @@ def part2():
     while True:
         if all([all([i == 0 for i in l]) for l in grid]):
             break
-
         cnt += 1
-        trig = set()
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                grid[x][y] += 1
-                if grid[x][y] > 9:
-                    ready.append((x, y))
-                    trig.add((x, y))
-
-        while ready:
-            nx, ny = ready.popleft()
-            for dx, dy in deltas:
-                if 0 <= nx+dx < len(grid) and 0 <= ny+dy < len(grid[0]):
-                    grid[nx+dx][ny+dy] += 1
-                    if grid[nx+dx][ny+dy] > 9 and (nx+dx, ny+dy) not in trig:
-                        ready.append((nx+dx, ny+dy))
-                        trig.add((nx+dx, ny+dy))
-
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] > 9:
-                    grid[x][y] = 0
+        cycle(grid)
 
     print(cnt)
 
