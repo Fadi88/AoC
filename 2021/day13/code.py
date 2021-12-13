7cbbf677a10acaf0f3648342404df1eedeba5487
Fadi Sedik <fady88@gmail.com>
2021-12-13 06:35:25 +0100

    adding day 13 2021 python

--- 2021/day13/code.py
+++ 2021/day13/code.py
@@ -13,12 +13,74 @@ def profiler(method):
 
 @profiler
 def part1():
-    pass
+    dots = set()
+    fold = []
+    for l in open("day13/input.txt"):
+        if "," in l:
+            dots.add(tuple(map(int, l.strip().split(','))))
+        elif 'fold' in l:
+            p = l.split('=')
+            fold.append(tuple((p[0][-1], int(p[1]))))
+
+    for axe, loc in fold:
+        new_dots = set()
+        if axe == 'x':
+            for p in dots:
+                if p[0] > loc:
+                    new_dots.add((loc - (p[0] - loc), p[1]))
+                else:
+                    new_dots.add(p)
+        else:
+            for p in dots:
+                if p[1] > loc:
+                    new_dots.add((p[0], loc - (p[1] - loc)))
+                else:
+                    new_dots.add(p)
+
+        dots = new_dots
+
+        print(len(dots))
+        break
 
 
 @profiler
 def part2():
-    pass
+    dots = set()
+    fold = []
+    for l in open("day13/input.txt"):
+        if "," in l:
+            dots.add(tuple(map(int, l.strip().split(','))))
+        elif 'fold' in l:
+            p = l.split('=')
+            fold.append(tuple((p[0][-1], int(p[1]))))
+
+    for axe, loc in fold:
+        new_dots = set()
+        if axe == 'x':
+            for p in dots:
+                if p[0] > loc:
+                    new_dots.add((loc - (p[0] - loc), p[1]))
+                else:
+                    new_dots.add(p)
+        else:
+            for p in dots:
+                if p[1] > loc:
+                    new_dots.add((p[0], loc - (p[1] - loc)))
+                else:
+                    new_dots.add(p)
+
+        dots = new_dots
+
+    max_x = max([p[0] for p in dots])
+    max_y = max([p[1] for p in dots])
+
+    for y in range(max_y + 1):
+        for x in range(max_x + 1):
+            if (x, y) in dots:
+                print("x", end="")
+            else:
+                print(" ", end="")
+        print()
 
 
 if __name__ == "__main__":
