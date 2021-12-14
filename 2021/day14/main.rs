231e36509ae9bccc9f129b1f0a5b7af8fe7d9785
Fadi Sedik <fady88@gmail.com>
2021-12-14 16:23:30 +0100

    day 14 2021 rust port

--- 2021/day14/main.rs
+++ 2021/day14/main.rs
@@ -39,16 +39,82 @@ fn part_1() {
         formula = new_forumla;
     }
 
-    let mut freq : HashMap<char,u16> = HashMap::new();
+    let mut freq: HashMap<char, u16> = HashMap::new();
 
-    for i in HashSet::<char>::from_iter(formula.chars()){
+    for i in HashSet::<char>::from_iter(formula.chars()) {
         freq.insert(i, formula.matches(i).count() as u16);
     }
 
-    println!("{:?}" , freq.values().max().unwrap() - freq.values().min().unwrap() ) ;
+    println!(
+        "part 1 : {:?}",
+        freq.values().max().unwrap() - freq.values().min().unwrap()
+    );
 }
 
-fn part_2() {}
+fn part_2() {
+    let mut input = include_str!("input.txt").split("\n\n");
+
+    let formula = String::from(input.next().unwrap());
+
+    let mut trans: HashMap<String, char> = HashMap::new();
+
+    for l in input.next().unwrap().split("\n") {
+        let mut tmp = l.split(" -> ");
+        trans.insert(
+            String::from(tmp.next().unwrap()),
+            tmp.next().unwrap().chars().next().unwrap(),
+        );
+    }
+
+    let mut pairs: HashMap<String, u64> = HashMap::new();
+    for i in 0..formula.len() - 1 {
+        *pairs
+            .entry(formula.chars().skip(i).take(2).collect::<String>())
+            .or_insert(0) += 1;
+    }
+
+    for _ in 0..40 {
+        let mut new_pairs: HashMap<String, u64> = HashMap::new();
+
+        for i in &pairs {
+            let mut k1: String = String::new();
+            let mut k2: String = String::new();
+            k1.push(i.0.chars().nth(0).unwrap());
+            k1.push(trans[i.0]);
+            k2.push(trans[i.0]);
+            k2.push(i.0.chars().nth(1).unwrap());
+
+            *new_pairs.entry(k1).or_insert(0) += *i.1;
+            *new_pairs.entry(k2).or_insert(0) += *i.1;
+        }
+
+        pairs = new_pairs;
+    }
+
+    let mut freq: HashMap<char, u64> = HashMap::new();
+
+    for p in &pairs {
+        *freq.entry(p.0.chars().nth(0).unwrap()).or_insert(0) += p.1;
+        *freq.entry(p.0.chars().nth(1).unwrap()).or_insert(0) += p.1;
+    }
+
+    *freq.entry(formula.chars().nth(0).unwrap()).or_insert(0) += 1;
+    *freq
+        .entry(formula.chars().nth_back(0).unwrap())
+        .or_insert(0) += 1;
+
+    println!(
+        "{:?}",
+        (freq.values().max().unwrap() - freq.values().min().unwrap()) / 2
+    );
+
+    println!("{:?}", freq);
+    println!(
+        "{} {}",
+        freq.values().max().unwrap(),
+        freq.values().min().unwrap()
+    )
+}
 
 fn main() {
     bench(part_1);
