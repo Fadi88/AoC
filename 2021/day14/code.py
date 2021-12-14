72ffba8b2b49af7b43be887fef1ca118a09e0bd1
Fadi Sedik <fady88@gmail.com>
2021-12-14 07:07:10 +0100

    day 15 2021 python

--- 2021/day14/code.py
+++ 2021/day14/code.py
@@ -1,4 +1,5 @@
 import time
+from collections import Counter, defaultdict
 
 
 def profiler(method):
@@ -19,12 +20,54 @@ def profiler(method):
 
 @profiler
 def part1():
-    pass
+    p = open("day14/input.txt").read().split("\n\n")
+
+    formula = list(p[0])
+    trans = {l.split(" -> ")[0]: l.split(" -> ")[1] for l in p[1].split("\n")}
+
+    for _ in range(10):
+        new_formula = []
+        for i in range(len(formula) - 1):
+            k = "".join([formula[i], formula[i + 1]])
+            new_formula += [formula[i], trans[k]]
+
+        new_formula.append(formula[-1])
+        formula = new_formula
+
+    freq = sorted(Counter(formula).values())
+    print(freq[-1] - freq[0])
 
 
 @profiler
 def part2():
-    pass
+    input = open("day14/input.txt").read().split("\n\n")
+
+    trans = {l.split(" -> ")[0]: l.split(" -> ")[1] for l in input[1].split("\n")}
+
+    pairs = {p: 0 for p in trans}
+
+    for i in range(len(input[0]) - 1):
+        pairs["".join([input[0][i], input[0][i + 1]])] += 1
+
+    for _ in range(40):
+        new_pairs = {p: 0 for p in trans}
+        for p in pairs:
+            new_pairs[p[0] + trans[p]] += pairs[p]
+            new_pairs[trans[p] + p[1]] += pairs[p]
+
+        pairs = new_pairs
+
+    freq = defaultdict(int)
+
+    for p in pairs:
+        freq[p[0]] += pairs[p]
+        freq[p[1]] += pairs[p]
+
+    # all chars are counted twice except for the first one and last one
+    freq[input[0][0]] += 1
+    freq[input[0][-1]] += 1
+
+    print((max(freq.values()) - min(freq.values())) // 2)
 
 
 if __name__ == "__main__":
