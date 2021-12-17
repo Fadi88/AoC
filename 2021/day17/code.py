5c78b5aec7b7a9f600fd4287bb404abd712cb028
Fadi Sedik <fady88@gmail.com>
2021-12-17 18:47:13 +0100

    testing new pylint style

--- 2021/day17/code.py
+++ 2021/day17/code.py
@@ -5,67 +5,73 @@ import math
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
-        t = time.time()
+        t_0 = time.time()
         ret = method(*arg, **kw)
-        print(
-            "Method "
-            + method.__name__
-            + " took : "
-            + "{:2.5f}".format(time.time() - t)
-            + " sec"
-        )
+        new_t = time.time() - t_0
+        print("Method " + method.__name__ + " took : " + f"{new_t:2.5f}" + " sec")
         return ret
 
     return wrapper_method
 
 
-def get_pos(vx0, vy0, t):
-    y = vy0 * t - (t - 1) * (t) // 2
+def get_pos(vx0, vy0, current_time):
+    current_y = vy0 * current_time - (current_time - 1) * (current_time) // 2
 
     # ignore -ve x as the input is to the right
-    x = (2 * vx0 - t + 1) * (t) // 2 if t < vx0 else vx0 * (vx0 + 1) // 2
+    current_x = (
+        (2 * vx0 - current_time + 1) * (current_time) // 2
+        if current_time < vx0
+        else vx0 * (vx0 + 1) // 2
+    )
 
-    return x, y
+    return current_x, current_y
 
 
-def will_intersect(v, u, l):
+def will_intersect(current_v, upper_bound, lower_bound):
     # get time limit based on y because it is easier to get than x
     # if it works for that time slot for y should also work x
     # because they both has to match
-    tmin = math.floor(v[1] + math.sqrt(v[1] * v[1] - 2 * u[1]))
-    tmax = math.floor(v[1] + math.sqrt(v[1] * v[1] - 2 * l[1]))
+    tmin = math.floor(
+        current_v[1] + math.sqrt(current_v[1] * current_v[1] - 2 * upper_bound[1])
+    )
+    tmax = math.floor(
+        current_v[1] + math.sqrt(current_v[1] * current_v[1] - 2 * lower_bound[1])
+    )
 
-    for t in range(tmin, tmax + 2):
-        x, y = get_pos(v[0], v[1], t)
-        if u[0] <= x <= l[0] and l[1] <= y <= u[1]:
+    for current_t in range(tmin, tmax + 2):
+        current_x, current_y = get_pos(current_v[0], current_v[1], current_t)
+        if (
+            upper_bound[0] <= current_x <= lower_bound[0]
+            and lower_bound[1] <= current_y <= upper_bound[1]
+        ):
             return True
     return False
 
 
 @profiler
 def part1():
-    _, _, y1, _ = list(map(int, re.findall(r"-?\d+", open("day17/input.txt").read())))
+    with open("day17/input.txt", encoding="utf-8") as f_d:
+        _, _, y_1, _ = list(map(int, re.findall(r"-?\d+", f_d.read())))
 
-    print(abs(y1) * (abs(y1) - 1) // 2)
+    print(abs(y_1) * (abs(y_1) - 1) // 2)
 
 
 @profiler
 def part2():
-    x1, x2, y1, y2 = list(
-        map(int, re.findall(r"-?\d+", open("day17/input.txt").read()))
-    )
+    with open("day17/input.txt", encoding="utf-8") as f_d:
+        x_1, x_2, y_1, y_2 = list(map(int, re.findall(r"-?\d+", f_d.read())))
 
     # assume area is under y = 0
 
     # bounds if exceeded after 1 second will overshoot
-    vy_min = y1
-    vx_max = x2
+    vy_min = y_1
+    vx_max = x_2
 
-    vy_max = -y1
-    vx_min = math.floor(math.sqrt(2 * x1) - 1)
+    vy_max = -y_1
+    vx_min = math.floor(math.sqrt(2 * x_1) - 1)
 
-    upper_bound = (x1, y2)
-    lower_bound = (x2, y1)
+    upper_bound = (x_1, y_2)
+    lower_bound = (x_2, y_1)
 
     tmp = sum(
         [
