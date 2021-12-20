bc49505f54c069fe7eb219b0332b73899f8d2208
Fadi Sedik <fady88@gmail.com>
2021-12-20 12:21:43 +0100

    minors edits python day 20 2021

--- 2021/day20/code.py
+++ 2021/day20/code.py
@@ -17,9 +17,10 @@ def profiler(method):
 
     return wrapper_method
 
-
-@profiler
-def part1():
+def sim_cycles(cycles):
+    
+    assert(cycles%2 == 0)
+    
     p = open("day20/input.txt").read().split("\n\n")
 
     algo = p[0].replace("\n", "")
@@ -31,12 +32,14 @@ def part1():
             if c == "#":
                 lit_pix.add((x, y))
 
-    for iter in range(2):
+    for iter in range(cycles):
         new_pix = set()
-        x_l = min([p[0] for p in lit_pix])
-        x_u = max([p[0] for p in lit_pix])
-        y_l = min([p[1] for p in lit_pix])
-        y_u = max([p[1] for p in lit_pix])
+
+        xs = [p[0] for p in lit_pix]
+        x_l , x_u = min(xs) , max(xs)
+
+        ys = [p[1] for p in lit_pix]
+        y_l , y_u = min(ys) , max(ys)
 
         for x in range(x_l - 1, x_u + 2):
             for y in range(y_l - 1, y_u + 2):
@@ -53,46 +56,16 @@ def part1():
                     new_pix.add((x, y))
         lit_pix = new_pix
 
-    print(len(lit_pix))
+    return len(lit_pix)
+
+@profiler
+def part1():
+    print(sim_cycles(2))
 
 
 @profiler
 def part2():
-    p = open("day20/input.txt").read().split("\n\n")
-
-    algo = p[0].replace("\n", "")
-    i = p[1].split("\n")
-
-    lit_pix = set()
-    for y, l in enumerate(i):
-        for x, c in enumerate(l):
-            if c == "#":
-                lit_pix.add((x, y))
-
-    for iter in range(50):
-        new_pix = set()
-        x_l = min([p[0] for p in lit_pix])
-        x_u = max([p[0] for p in lit_pix])
-        y_l = min([p[1] for p in lit_pix])
-        y_u = max([p[1] for p in lit_pix])
-
-        for x in range(x_l - 1, x_u + 2):
-            for y in range(y_l - 1, y_u + 2):
-                idx = ""
-                for t in range(9):
-                    nx = x + t % 3 - 1
-                    ny = y + t // 3 - 1
-                    if x_l <= nx <= x_u and  y_l <= ny <= y_u :
-                        idx += '1' if (nx,ny) in lit_pix else '0'
-                    else :
-                        idx += '1' if iter%2 == 1 else '0'
-
-                if algo[int(idx, 2)] == "#":
-                    new_pix.add((x, y))
-        lit_pix = new_pix
-
-    print(len(lit_pix))
-  
+    print(sim_cycles(50))
 
 if __name__ == "__main__":
 
