389e45ebf006dff1b715159be1f393826e2aa189
Fadi Sedik <fady88@gmail.com>
2021-12-22 12:04:58 +0100

    day 22 2021 part 1 in rust ready

--- 2021/day22/main.rs
+++ 2021/day22/main.rs
@@ -1,3 +1,6 @@
+use regex::Regex;
+use std::cmp::{max, min};
+use std::collections::HashSet;
 use std::time;
 
 fn bench(f: fn()) {
@@ -9,7 +12,35 @@ fn bench(f: fn()) {
 }
 
 fn part_1() {
-    include_str!("input.txt");
+    let re = Regex::new(r".*=(-?\d+)\.+(-?\d+).*=(-?\d+)\.+(-?\d+).*=(-?\d+)\.+(-?\d+)").unwrap();
+    let mut pts: HashSet<(i32, i32, i32)> = HashSet::new();
+
+    for l in include_str!("input.txt").split("\n") {
+        let t: Vec<_> = re
+            .captures(l)
+            .unwrap()
+            .iter()
+            .skip(1)
+            .map(|x| x.unwrap().as_str().parse::<i32>().unwrap())
+            .collect();
+
+        if t[0] > 50 || t[1] < -50 || t[2] > 50 || t[3] < -50 || t[4] > 50 || t[5] < -50 {
+            continue;
+        }
+
+        for x in max(t[0], -50)..min(50, t[1] + 1) {
+            for y in max(t[2], -50)..min(50, t[3] + 1) {
+                for z in max(t[4], -50)..min(50, t[5] + 1) {
+                    if l.contains("on") {
+                        pts.insert((x, y, z));
+                    } else {
+                        pts.remove(&(x, y, z));
+                    }
+                }
+            }
+        }
+    }
+    println!("{}" , pts.len());
 }
 
 fn part_2() {}
