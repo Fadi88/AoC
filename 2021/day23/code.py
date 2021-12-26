cf1cdf57a7f06b3a70314e0a2c5247adc3596caa
Fadi Sedik <fady88@gmail.com>
2021-12-27 00:27:55 +0100

    adding day 23 2021 python
    runtime 7s+ need to cut it down somehow
    maybe due to vector copy maybe try string instead

--- 2021/day23/code.py
+++ 2021/day23/code.py
@@ -1,10 +1,8 @@
-from os import stat
 import time
-from queue import PriorityQueue as pq
-from collections import defaultdict
 import math
+from queue import PriorityQueue as pq
 from copy import deepcopy
-from functools import lru_cache
+from collections import defaultdict
 
 
 def profiler(method):
@@ -23,103 +21,147 @@ def profiler(method):
     return wrapper_method
 
 
-def print_state(s):
-    for l in s:
-        print("".join(l))
+def get_possible_states(state):
+    ret = []
 
+    room_size = len(state) - 2
+
+    rooms_char = {3: "A", 5: "B", 7: "C", 9: "D"}
+    chars_pos = {"A": 3, "B": 5, "C": 7, "D": 9}
+    cost = {"A": 1, "B": 10, "C": 100, "D": 1000}
+    hallway_spot = [1, 2, 4, 6, 8, 10, 11]
+
+    for x in hallway_spot:
+        if state[1][x] in "ABCD":
+            x_target = chars_pos[state[1][x]]
+            if all(
+                [
+                    state[y][x_target] == "." or state[y][x_target] == state[1][x]
+                    for y in range(2, 2 + room_size - 1)
+                ]
+            ):
+
+                x_range = sorted([x, x_target])
+                if all(
+                    [
+                        state[1][n_x] == "." or state[1][n_x] == state[1][x]
+                        for n_x in range(x_range[0], x_range[1] + 1)
+                    ]
+                ):
+                    y_target = max(
+                        [
+                            n_y
+                            for n_y in range(2, 2 + room_size - 1)
+                            if state[n_y][x_target] == "."
+                        ]
+                    )
+                    n_state = deepcopy(state)
+                    n_state[y_target][x_target] = state[1][x]
+                    n_state[1][x] = "."
+                    yield (
+                        (x_range[1] - x_range[0] + y_target - 1) * cost[state[1][x]],
+                        n_state,
+                    )
+
+    # create state where we push amphipod out of the rooms
+    for x in [3, 5, 7, 9]:
+        if all(
+            [
+                state[y][x] == "." or state[y][x] == rooms_char[x]
+                for y in range(2, 2 + room_size - 1)
+            ]
+        ):
+            continue
+
+        for y in range(2, 2 + room_size - 1):
+            if state[y][x] != ".":
+                if all(
+                    [
+                        state[n_y][x] == rooms_char[x]
+                        for n_y in range(y, 2 + room_size - 1)
+                    ]
+                ):
+                    break
+                for n_x in list(filter(lambda c: c < x, hallway_spot))[::-1]:
+                    if state[1][n_x] == ".":
+                        n_state = deepcopy(state)
+                        n_state[1][n_x] = state[y][x]
+                        n_state[y][x] = "."
+                        yield ((y - 1 + x - n_x) * cost[state[y][x]], n_state)
+                    else:
+                        break
+                for n_x in list(filter(lambda c: c > x, hallway_spot)):
+                    if state[1][n_x] == ".":
+                        n_state = deepcopy(state)
+                        n_state[1][n_x] = state[y][x]
+                        n_state[y][x] = "."
+                        yield ((y - 1 + n_x - x) * cost[state[y][x]], n_state)
+                    else:
+                        break
+                break
+
+
+def print_state(state):
+    for l in state:
+        print("".join(l))
     print()
 
+def get_state_string(state):
+    st = "".join(state[1][1:-1])
+    for x in [3, 5, 7, 9]:
+        for y in range(2,len(state) - 1):
+            st += state[y][x]
 
-def get_possible_states(state):
-    deltas = [(-1, 0), (1, 0), (1, -1), (-1, -1), (0, -1), (0, 1), (1, 1), (-1, 1)]
-
-    for y, l in enumerate(state):
-        for x, c in enumerate(l):
-            if c in ["A", "B", "C", "D"]:
-                for dx, dy in deltas:
-                    if state[y + dy][x + dx] == ".":
-                        if (x + dx, y + dy) in [(9, 1), (7, 1), (5, 1), (3, 1)]:
-                            continue
-                        n_state = deepcopy(state)
-                        n_state[y + dy][x + dx] = c
-                        n_state[y][x] = "."
-
-                        cost = (abs(dx) + abs(dy)) * 10 ** (ord(c) - ord("A"))
-
-                        yield (cost, n_state)
-
+    return st
 
 @profiler
 def part1():
     state = [list(l.rstrip()) for l in open("day23/input.txt")]
 
-    f_str = (
-        "#############\n"
-        + "#...........#\n"
-        + "###A#B#C#D###\n"
-        + "  #A#B#C#D#\n"
-        + "  #########"
-    )
-    f_state = [list(l) for l in f_str.split("\n")]
-    visited = set()
+    f_str = "...........AABBCCDD"
+
     visit = pq()
     visit.put((0, state))
 
-    cost = defaultdict(lambda: math.inf)
-    cost[str(state)] = 0
-
-    prev = {}
+    cost = defaultdict(lambda : math.inf)
 
     while not visit.empty():
-        c, c_state = visit.get()
+        c_c, c_s = visit.get()
 
-        visited.add(str(c_state))
+        if get_state_string(c_s) == f_str:
+            break
+        for n_c, n_s in get_possible_states(c_s):
+            if cost[get_state_string(n_s)] > n_c + c_c:
+                cost[get_state_string(n_s)] = n_c + c_c
+                visit.put((c_c + n_c, n_s))
 
-        for n_c, n_state in get_possible_states(c_state):
-            if n_c < cost[str(n_state)]:
-                cost[str(n_state)] = n_c + c
-                prev[str(n_state)] = str(c_state)
-            if str(n_state) not in visited:
-                visit.put((cost[str(n_state)], n_state))
+    print(cost[f_str])
 
 
 @profiler
 def part2():
     state = [list(l.rstrip()) for l in open("day23/input.txt")]
-    state = state[:3] + [list("  #D#C#B#A#")] + [list("  #D#B#A#C#")] + state[3:]
 
-    f_str = (
-        "#############\n"
-        + "#...........#\n"
-        + "###A#B#C#D###\n"
-        + "  #A#B#C#D#\n"
-        + "  #A#B#C#D#\n"
-        + "  #A#B#C#D#\n"
-        + "  #########"
-    )
-    f_state = [list(l) for l in f_str.split("\n")]
-    visited = set()
+    state = state[:3] + [list("  #D#C#B#A#")] + [list("  #D#B#A#C#")] + state[-2:]
+    
+    f_str = "...........AAAABBBBCCCCDDDD"
+
     visit = pq()
     visit.put((0, state))
 
-    cost = defaultdict(lambda: math.inf)
-    cost[str(state)] = 0
-
-    prev = {}
+    cost = defaultdict(lambda : math.inf)
 
     while not visit.empty():
-        c, c_state = visit.get()
+        c_c, c_s = visit.get()
 
-        visited.add(str(c_state))
+        if get_state_string(c_s) == f_str:
+            break
+        for n_c, n_s in get_possible_states(c_s):
+            if cost[get_state_string(n_s)] > n_c + c_c:
+                cost[get_state_string(n_s)] = n_c + c_c
+                visit.put((c_c + n_c, n_s))
 
-        for n_c, n_state in get_possible_states(c_state):
-            if n_c < cost[str(n_state)]:
-                cost[str(n_state)] = n_c + c
-                prev[str(n_state)] = str(c_state)
-            if str(n_state) not in visited:
-                visit.put((cost[str(n_state)], n_state))
-
-    print(cost[str(f_state)])
+    print(cost[f_str])
 
 
 if __name__ == "__main__":
