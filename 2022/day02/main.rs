0a86f2515f6614319555fa423608b3467086cca2
Fadi Sedik <fady88@gmail.com>
2022-12-02 13:03:01 +0100

    day 02 2022 in rust both parts

--- 2022/day02/main.rs
+++ 2022/day02/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::time;
 
 fn bench(f: fn()) {
@@ -9,17 +10,46 @@ fn bench(f: fn()) {
 }
 
 fn part_1() {
-    for l in include_str!("input.txt").split("\n\n"){
-
+    let mut freq: HashMap<&str, u16> = HashMap::new();
+    for l in include_str!("input.txt").split("\n") {
+        *freq.entry(l).or_default() += 1;
     }
 
+    let mut score: u16 = 0;
+    for k in freq {
+        let hands: Vec<_> = k.0.split(' ').collect();
+
+        let h0 = hands[0].chars().next().unwrap() as u16 - 'A' as u16;
+        let h1 = hands[1].chars().next().unwrap() as u16 - 'X' as u16;
+
+        // game score = value of hand(R1,P2,S3) + 3*(L0,T1,W2)
+        let g_score = (h1 + 1) + (3 * (((h1 as i8 - h0 as i8) + 4) % 3)) as u16;
+
+        score += (g_score as u16 * k.1) as u16;
+    }
+
+    println!("{}", score);
 }
 
 fn part_2() {
-    for l in include_str!("input.txt").split("\n\n"){
-
+    let mut freq: HashMap<&str, u16> = HashMap::new();
+    for l in include_str!("input.txt").split("\n") {
+        *freq.entry(l).or_default() += 1;
     }
 
+    let mut score: u16 = 0;
+    for k in freq {
+        let hands: Vec<_> = k.0.split(' ').collect();
+
+        let h0 = hands[0].chars().next().unwrap() as u16 - 'A' as u16;
+        let h1 = hands[1].chars().next().unwrap() as u16 - 'X' as u16;
+
+        let g_score = h1 * 3 + (h0 as i8 + h1 as i8 +3 -1) as u16 % 3 + 1 ;
+
+        score += g_score * k.1;
+    }
+
+    println!("{}", score);
 }
 
 fn main() {
