9f78ea552456e3ff471e617ce193b6f0ef6d3022
Fadi Sedik <fady88@gmail.com>
2022-12-04 07:20:38 +0100

    adding day 04 2022 in rust

--- 2022/day04/main.rs
+++ 2022/day04/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::time;
 
 fn bench(f: fn()) {
@@ -8,11 +9,57 @@ fn bench(f: fn()) {
     ret
 }
 
-fn part_1() {
-    include_str!("input.txt");
+fn is_fully_contained(re: &Regex, s: &str) -> bool {
+    let ds = re
+        .captures_iter(s)
+        .map(|x| x.get(0).unwrap().as_str().parse::<u8>().unwrap())
+        .collect::<Vec<u8>>();
+    if (ds[0] <= ds[2] && ds[1] >= ds[3]) || (ds[2] <= ds[0] && ds[3] >= ds[1]) {
+        true
+    } else {
+        false
+    }
 }
 
-fn part_2() {}
+fn part_1() {
+    let re = Regex::new(r"\d+").unwrap();
+
+    println!(
+        "{}",
+        include_str!("input.txt")
+            .split("\n")
+            .filter(|x| is_fully_contained(&re, x))
+            .count()
+    );
+}
+
+fn is_overlapping(re: &Regex, s: &str) -> bool {
+    let ds = re
+        .captures_iter(s)
+        .map(|x| x.get(0).unwrap().as_str().parse::<u8>().unwrap())
+        .collect::<Vec<u8>>();
+    if (ds[2] >= ds[0] && ds[2] <= ds[1])
+        || (ds[3] >= ds[0] && ds[3] <= ds[1])
+        || (ds[0] >= ds[2] && ds[0] <= ds[3])
+        || (ds[1] >= ds[2] && ds[1] <= ds[3])
+    {
+        true
+    } else {
+        false
+    }
+}
+
+fn part_2() {
+    let re = Regex::new(r"\d+").unwrap();
+
+    println!(
+        "{}",
+        include_str!("input.txt")
+            .split("\n")
+            .filter(|x| is_overlapping(&re, x))
+            .count()
+    );
+}
 
 fn main() {
     bench(part_1);
