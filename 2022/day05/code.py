ffd173bcaf5b2455170fed441af833553f60c496
Fadi Sedik <fady88@gmail.com>
2022-12-05 06:31:35 +0100

    day05 2022 python

--- 2022/day05/code.py
+++ 2022/day05/code.py
@@ -1,4 +1,5 @@
 import time
+from collections import defaultdict
 
 
 def profiler(method):
@@ -13,12 +14,50 @@ def profiler(method):
 
 @profiler
 def part1():
-    pass
+    cargo = defaultdict(list)
+    parts = open("input.txt").read().split("\n\n")
+    config = parts[0].split('\n')
+
+    for l in config[:-1]:
+        for i, c in enumerate(l):
+            if c.isalpha():
+                cargo[config[-1][i]].insert(0, c)
+
+    for l in parts[1].split("\n"):
+        move = list(l.replace("move", "").replace(
+            "to", "").replace("from", "").split())
+
+        cargo[move[2]] += cargo[move[1]][-int(move[0]):][::-1]
+        del cargo[move[1]][-int(move[0]):]
+
+    res = ""
+    for i in range(1, 10):
+        res += cargo[str(i)][-1]
+    print(res)
 
 
 @profiler
 def part2():
-    pass
+    cargo = defaultdict(list)
+    parts = open("input.txt").read().split("\n\n")
+    config = parts[0].split('\n')
+
+    for l in config[:-1]:
+        for i, c in enumerate(l):
+            if c.isalpha():
+                cargo[config[-1][i]].insert(0, c)
+
+    for l in parts[1].split("\n"):
+        move = list(l.replace("move", "").replace(
+            "to", "").replace("from", "").split())
+
+        cargo[move[2]] += cargo[move[1]][-int(move[0]):]
+        del cargo[move[1]][-int(move[0]):]
+
+    res = ""
+    for i in range(1, 10):
+        res += cargo[str(i)][-1]
+    print(res)
 
 
 if __name__ == "__main__":
