01e5d5f181d1896966c8c5d7a666983590e23d77
Fadi Sedik <fady88@gmail.com>
2022-12-06 06:23:16 +0100

    adding rust for day 06 2022

--- 2022/day06/main.rs
+++ 2022/day06/main.rs
@@ -1,4 +1,5 @@
 use std::time;
+use std::collections::HashSet;
 
 fn bench(f: fn()) {
     let t0 = time::Instant::now();
@@ -9,10 +10,46 @@ fn bench(f: fn()) {
 }
 
 fn part_1() {
-    include_str!("input.txt");
+    let l = include_str!("input.txt").chars().collect::<Vec<_>>();
+
+    let target_len = 4;
+
+    for i in target_len-1..l.len(){
+
+        let mut message :HashSet<char> = HashSet::new();
+
+        for c in 0..target_len{
+            message.insert(l[i-c]);
+        }
+
+        if message.len() == target_len{
+            println!("{}" , i+1);
+            break
+        }
+
+    }
 }
 
-fn part_2() {}
+fn part_2() {
+    let l = include_str!("input.txt").chars().collect::<Vec<_>>();
+
+    let target_len = 14;
+
+    for i in target_len-1..l.len(){
+
+        let mut message :HashSet<char> = HashSet::new();
+
+        for c in 0..target_len{
+            message.insert(l[i-c]);
+        }
+
+        if message.len() == target_len{
+            println!("{}" , i+1);
+            break
+        }
+
+    }
+}
 
 fn main() {
     bench(part_1);
