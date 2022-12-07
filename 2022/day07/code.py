382848acc39ebac1d54a014af9487176f17b9f4a
Fadi Sedik <fady88@gmail.com>
2022-12-07 07:54:29 +0100

    adding day 07 2022 python

--- 2022/day07/code.py
+++ 2022/day07/code.py
@@ -1,4 +1,6 @@
 from time import perf_counter
+from collections import defaultdict
+from pathlib import Path
 
 
 def profiler(method):
@@ -13,12 +15,78 @@ def profiler(method):
 
 @profiler
 def part1():
-    pass
+    current_path = ""
+    file_size = {}
+
+    for l in open("input.txt").readlines():
+        if "$" in l:
+            if " cd " in l:
+                if l.split()[2] not in ["..", "/"]:
+                    current_path += ("/" if current_path !=
+                                     "/" else "") + l.split()[2]
+                elif l.split()[2] == "..":
+                    if current_path.count("/") > 0:
+                        current_path = current_path.rsplit("/", 1)[0]
+                elif l.split()[2] == "/":
+                    current_path = "/"
+        else:
+            if "dir" not in l:
+                file_size[(current_path + "/" if len(current_path) >
+                           1 else "/") + l.split()[1]] = int(l.split()[0])
+
+    folder_size = defaultdict(int)
+    for f in file_size:
+        folder = Path(f).parent
+        while True:
+            folder_size[folder] += file_size[f]
+            if folder == Path("/"):
+                break
+            folder = folder.parent
+
+    print(sum(filter(lambda x: x <= 100000, folder_size.values())))
 
 
 @profiler
 def part2():
-    pass
+    current_path = ""
+    file_size = {}
+
+    for l in open("input.txt").readlines():
+        if "$" in l:
+            if " cd " in l:
+                if l.split()[2] not in ["..", "/"]:
+                    current_path += ("/" if current_path !=
+                                     "/" else "") + l.split()[2]
+                elif l.split()[2] == "..":
+                    if current_path.count("/") > 0:
+                        current_path = current_path.rsplit("/", 1)[0]
+                elif l.split()[2] == "/":
+                    current_path = "/"
+        else:
+            if "dir" not in l:
+                file_size[(current_path + "/" if len(current_path) >
+                           1 else "/") + l.split()[1]] = int(l.split()[0])
+
+    folder_size = defaultdict(int)
+    for f in file_size:
+        folder = Path(f).parent
+        while True:
+            folder_size[folder] += file_size[f]
+            if folder == Path("/"):
+                break
+            folder = folder.parent
+
+    
+    total_space = 70000000
+    needed_space = 30000000
+
+    least_del = needed_space - (total_space - folder_size[Path("/")])
+
+
+    for s in sorted(folder_size.values()):
+        if s > least_del:
+            print(s)
+            break
 
 
 if __name__ == "__main__":
