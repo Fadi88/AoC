47824b79c8d870eac536dca902573b56c0dc2093
Fadi Sedik <fady88@gmail.com>
2022-12-11 07:46:46 +0100

    adding day11 2022

---
+++ 2022/day11/code.py
@@ -0,0 +1,126 @@
+from time import perf_counter
+
+
+def profiler(method):
+    def wrapper_method(*arg, **kw):
+        t = perf_counter()
+        ret = method(*arg, **kw)
+        print('Method ' + method.__name__ + ' took : ' +
+              "{:2.5f}".format(perf_counter()-t) + ' sec')
+        return ret
+    return wrapper_method
+
+
+class Monkey:
+    def __init__(self) -> None:
+        self.items = []
+        self.operation = tuple()
+        self.test = 0
+        self.if_true = 0
+        self.if_false = 0
+        self.activity = 0
+
+    def round(self):
+        ret = []
+        for i in self.items:
+            self.activity += 1
+            new_level = eval(
+                "i" + self.operation[0] + self.operation[1].replace("old", "i"))//3
+            ret.append((self.if_true if new_level %
+                       self.test == 0 else self.if_false, new_level))
+        self.items = []
+
+        return ret
+
+    def round_2(self):
+        ret = []
+        for i in self.items:
+            self.activity += 1
+            new_level = eval(
+                "i" + self.operation[0] + self.operation[1].replace("old", "i"))
+            ret.append((self.if_true if new_level %
+                       self.test == 0 else self.if_false, new_level))
+        self.items = []
+
+        return ret
+
+    def append_item(self,val):
+        self.items
+
+
+@profiler
+def part1():
+    monkeys = []
+
+    for m in open("input.txt").read().split("\n\n"):
+        tmp_monkey = Monkey()
+        for l in m.splitlines():
+            if "Starting items:" in l:
+                tmp_monkey.items += list(map(int, l.strip().replace(
+                    "Starting items: ", "").replace(",", "").split(" ")))
+            elif "Operation:" in l:
+                p = l.split()
+                tmp_monkey.operation = (p[-2], p[-1])
+            elif "Test:" in l:
+                tmp_monkey.test = int(l.split()[-1])
+            elif "If true:" in l:
+                tmp_monkey.if_true = int(l.split()[-1])
+            elif "If false:" in l:
+                tmp_monkey.if_false = int(l.split()[-1])
+
+        monkeys.append(tmp_monkey)
+
+    for _ in range(20):
+        for m in range(len(monkeys)):
+            tmp = monkeys[m].round()
+            for i in tmp:
+                monkeys[i[0]].items.append(i[1])
+    monkeys.sort(key=lambda x : x.activity)
+
+
+    print(monkeys[-1].activity * monkeys[-2].activity )
+    
+
+
+@profiler
+def part2():
+    monkeys = []
+
+    for m in open("input.txt").read().split("\n\n"):
+        tmp_monkey = Monkey()
+        for l in m.splitlines():
+            if "Starting items:" in l:
+                tmp_monkey.items += list(map(int, l.strip().replace(
+                    "Starting items: ", "").replace(",", "").split(" ")))
+            elif "Operation:" in l:
+                p = l.split()
+                tmp_monkey.operation = (p[-2], p[-1])
+            elif "Test:" in l:
+                tmp_monkey.test = int(l.split()[-1])
+            elif "If true:" in l:
+                tmp_monkey.if_true = int(l.split()[-1])
+            elif "If false:" in l:
+                tmp_monkey.if_false = int(l.split()[-1])
+
+        monkeys.append(tmp_monkey)
+
+    divisor = 1
+    for m in monkeys:
+        divisor *= m.test
+
+    for _ in range(10000):
+        for m in range(len(monkeys)):
+            tmp = monkeys[m].round_2()
+            for i in tmp:
+                monkeys[i[0]].items.append(i[1] % divisor)
+
+    monkeys.sort(key=lambda x : x.activity)
+
+
+    print(monkeys[-1].activity * monkeys[-2].activity )
+
+
+if __name__ == "__main__":
+
+    part1()
+    part2()
