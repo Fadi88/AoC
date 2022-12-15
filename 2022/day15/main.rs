68e956783e3cefd8dddc70386f02c61440c6a1f8
Fadi Sedik <fady88@gmail.com>
2022-12-15 09:22:37 +0100

    adding part 1 day 15 2022 in rust

--- 2022/day15/main.rs
+++ 2022/day15/main.rs
@@ -1,5 +1,5 @@
-use std::time;
 use std::fs;
+use std::time;
 
 fn bench(f: fn()) {
     let t0 = time::Instant::now();
@@ -9,8 +9,65 @@ fn bench(f: fn()) {
     ret
 }
 
+fn get_taxi_distance(p1: &(i32, i32), p2: &(i32, i32)) -> u32 {
+    p1.0.abs_diff(p2.0) + p1.1.abs_diff(p2.1)
+}
+
 fn part_1() {
-    fs::read_to_string("input.txt").unwrap().split("\n");
+    let mut grid: Vec<((i32, i32), (i32, i32))> = Vec::new();
+    for l in fs::read_to_string("input.txt").unwrap().split("\n") {
+        let ls = l
+            .replace("Sensor at ", "")
+            .replace(" closest beacon is at ", "")
+            .replace(" ", "")
+            .replace(":", ",")
+            .replace("x=", "")
+            .replace("y=", "");
+        let mut ps = ls.split(",").map(|x| x.parse::<i32>().unwrap());
+
+        grid.push((
+            (ps.next().unwrap(), ps.next().unwrap()),
+            (ps.next().unwrap(), ps.next().unwrap()),
+        ))
+    }
+
+    let mut confirmed_free: Vec<(i32, i32)> = Vec::new();
+    let target_y = 2000000;
+
+    for (sensor, beacon) in grid {
+        let b_s_dst = get_taxi_distance(&sensor, &beacon);
+
+        let dy = sensor.1.abs_diff(target_y);
+
+        if dy <= b_s_dst {
+            let dx = (b_s_dst - dy) as i32;
+            confirmed_free.push((sensor.0 - dx, sensor.0 + dx));
+        }
+    }
+    confirmed_free.sort();
+
+    let mut combined_ranges: Vec<(i32, i32)> = Vec::new();
+
+    combined_ranges.push(confirmed_free[0]);
+
+    for i in 1..confirmed_free.len() {
+        let r = confirmed_free[i];
+        let to_check = combined_ranges.last().unwrap();
+
+        if r.0 > to_check.1 {
+            combined_ranges.push(r);
+        } else {
+            *combined_ranges.last_mut().unwrap() =
+                (to_check.0, [r.1, to_check.1].iter().max().unwrap().clone());
+        }
+    }
+
+    let mut total: u32 = 0;
+    for r in combined_ranges {
+        total += (r.1 - r.0) as u32;
+    }
+
+    println!("{}", total);
 }
 
 fn part_2() {
