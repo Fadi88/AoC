f3309211822e858180a7508e4b1596ab4c2dc45c
Fadi Sedik <fady88@gmail.com>
2022-12-18 07:23:23 +0100

    day 18 part 2 python changed to flood fill

--- 2022/day18/code.py
+++ 2022/day18/code.py
@@ -33,14 +33,14 @@ def part1():
     print(exposed)
 
 
-def is_exposed(air, p):
-    target = (0, 0, 0)
+def fill(air):
+    start = (0, 0, 0)
 
     visited = set()
-    visited.add(p)
+    visited.add(start)
 
     to_visit = deque()
-    to_visit.append([p])
+    to_visit.append([start])
 
     xs = set(p[0] for p in air)
     ys = set(p[1] for p in air)
@@ -59,34 +59,36 @@ def is_exposed(air, p):
                 new_p = (last_p[0] + move[0], last_p[1] +
                          move[1], last_p[2] + move[2])
 
-                if new_p[0] < min(xs) - 1 or new_p[0] > max(xs) + 1:
+                if new_p in air:
                     continue
 
-                if new_p[1] < min(ys) - 1  or new_p[1] > max(ys) + 1:
+                if new_p[0] < -1 or new_p[0] > max(xs) + 1:
                     continue
 
-                if new_p[2] < min(zs) - 1  or new_p[2] > max(zs) + 1:
+                if new_p[1] < -1 or new_p[1] > max(ys) + 1:
+                    continue
+
+                if new_p[2] < -1 or new_p[2] > max(zs) + 1:
                     continue
 
                 if new_p not in current_path and new_p not in visited and new_p not in air:
                     new_path = current_path + [new_p]
-                    if new_p == target:
-                        return True,new_path
                     visited.add(new_p)
                     to_visit.append(new_path)
 
-    return False,None
-
+    return visited
 
 @profiler
 def part2():
+
     air = set()
     for l in open("input.txt"):
         air.add(tuple(map(int, l.split(","))))
 
     exposed = 0
 
-    cache = {}
+    exposed_surfaces = fill(air)
+
     for p in air:
         for d in range(3):
             for delta in [-1, 1]:
@@ -97,15 +99,7 @@ def part2():
                 if to_test in air:
                     continue
 
-                if to_test not in cache:
-                    state,path = is_exposed(air, to_test)
-                    if state:
-                        for np in path:
-                            cache[np] = True
-                    else:
-                        cache[to_test] = False
-
-                if cache[to_test]:
+                if to_test in exposed_surfaces:
                     exposed += 1
 
     print(exposed)
