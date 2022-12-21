bd80f2e1d0aca5729dbf3802677595d88f54902b
Fadi Sedik <fady88@gmail.com>
2022-12-21 07:35:48 +0100

    day 21 2022 python

--- 2022/day21/code.py
+++ 2022/day21/code.py
@@ -1,4 +1,5 @@
 from time import perf_counter
+import re
 
 
 def profiler(method):
@@ -11,15 +12,86 @@ def profiler(method):
     return wrapper_method
 
 
+def get_val(monkeys, key):
+    if monkeys[key].isnumeric():
+        return int(monkeys[key])
+
+    p = monkeys[key].split()
+    return eval(str(get_val(monkeys, p[0])) + p[1] + str(get_val(monkeys, p[2])))
+
+
 @profiler
 def part1():
-    pass
+    monkeys = {}
+    for l in open("input.txt").read().splitlines():
+        p = l.split(":")
+        monkeys[p[0]] = p[1].strip()
+
+    print(int(get_val(monkeys, "root")))
+
+
+def simplify(expr):
+    while True:
+        par = re.findall(r"\(-?\d+[*-\\+]-?\d+\)", expr)
+        if len(par) == 0:
+            return expr
+
+        for e in par:
+            expr = expr.replace(e, str(int(eval(e))))
+
+
+def get_eq(monkeys, key):
+    if key == "humn":
+        return key
+
+    if monkeys[key].isnumeric():
+        return monkeys[key]
+
+    p = monkeys[key].split()
+
+    if key == "root":
+        lhs = str(get_eq(monkeys, p[0]))
+        rhs = str(get_eq(monkeys, p[2]))
+
+        if "humn" not in lhs:
+            lhs = str(int(eval(lhs)))
+        else:
+            rhs = str(int(eval(rhs)))
+        return "(" + rhs + ") = (" + lhs + ")"
+    return "(" + str(get_eq(monkeys, p[0])) + p[1] + str(get_eq(monkeys, p[2])) + ")"
 
 
 @profiler
 def part2():
-    pass
+    monkeys = {}
+    for l in open("input.txt").read().splitlines():
+        p = l.split(":")
+        monkeys[p[0]] = p[1].strip()
 
+    eq = get_eq(monkeys, "root").split("=")
+
+    if "humn" in eq[0]:
+        rhs = eq[0]
+        lhs = eq[1]
+    else:
+        rhs = eq[1]
+        lhs = eq[0]
+
+    rhs = simplify(rhs).replace("/", "//")
+    lhs = lhs[1:-2]
+
+    r = [0, 1e15]
+
+    while True:
+        mid = sum(r)//2
+
+        if eval(rhs.replace("humn", str(int(mid)))) < int(lhs):
+            r[1] = mid
+        elif eval(rhs.replace("humn", str(int(mid)))) > int(lhs):
+            r[0] = mid
+        else:
+            print(int(mid))
+            break
 
 if __name__ == "__main__":
 
