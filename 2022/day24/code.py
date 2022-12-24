b69b0542c13df92843404b71b6c70dac759c0058
Fadi Sedik <fady88@gmail.com>
2022-12-24 12:34:17 +0100

    adding day 24 2022 in python

--- 2022/day24/code.py
+++ 2022/day24/code.py
@@ -1,4 +1,6 @@
-from time import perf_counter
+import cProfile
+from collections import deque
+from time import time as perf_counter
 
 
 def profiler(method):
@@ -11,14 +13,221 @@ def profiler(method):
     return wrapper_method
 
 
+class blizzard:
+    def __init__(self, direction, pos):
+        self.direction = direction
+        self.pos = pos
+
+    def __str__(self):
+        return str(self.pos) + self.direction + " "
+
+
+def cycle_grid(grid, max_x, max_y):
+    new_grid = set()
+    for b in grid:
+        x, y = b.pos
+        if b.direction == ">":
+            x = b.pos[0] + 1
+            if x >= max_x:
+                x = 1
+        elif b.direction == "<":
+            x = b.pos[0] - 1
+            if x == 0:
+                x = max_x - 1
+        elif b.direction == "^":
+            y = b.pos[1] - 1
+            if y == 0:
+                y = max_y - 1
+        elif b.direction == "v":
+            y = b.pos[1] + 1
+            if y >= max_y:
+                y = 1
+
+        new_grid.add(blizzard(b.direction, (x, y)))
+
+    return new_grid
+
+
 @profiler
 def part1():
-    pass
+    grid = set()
+
+    for y, l in enumerate(open("input.txt").read().splitlines()):
+        for x, c in enumerate(l):
+            if c in ["<", ">", "^", "v"]:
+                grid.add(blizzard(c, (x, y)))
+
+    max_x = x
+    max_y = y
+
+    target = (x-1, y)
+
+    start = (1, 0)
+
+    history = {0: grid}
+    occupied = {0: set(b.pos for b in history[0])}
+
+    to_visit = deque([(start, 0)])
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    seen = set()
+
+    while to_visit:
+        current_pos, current_step = to_visit.popleft()
+
+        if (current_pos, current_step) in seen:
+            continue
+
+        seen.add((current_pos, current_step))
+
+        if (current_step + 1) not in history:
+            history[current_step +
+                    1] = cycle_grid(history[current_step], max_x, max_y)
+            occupied[current_step +
+                     1] = set(b.pos for b in history[current_step + 1])
+
+        if current_pos not in occupied[current_step + 1]:
+            # wait for one minute if the place is blizzard free the next step
+            to_visit.append((current_pos, current_step + 1))
+
+        for d in deltas:
+            nx, ny = current_pos[0] + d[0], current_pos[1] + d[1]
+
+            if (nx, ny) == target:
+                print(current_step+1)
+                return
+
+            if 0 < nx < max_x and 0 < ny < max_y:
+                if (nx, ny) not in occupied[current_step + 1]:
+                    to_visit.append(((nx, ny), current_step + 1))
 
 
 @profiler
 def part2():
-    pass
+    grid = set()
+
+    for y, l in enumerate(open("input.txt").read().splitlines()):
+        for x, c in enumerate(l):
+            if c in ["<", ">", "^", "v"]:
+                grid.add(blizzard(c, (x, y)))
+
+    max_x = x
+    max_y = y
+
+    target = (x-1, y)
+
+    start = (1, 0)
+
+    history = {0: grid}
+    occupied = {0: set(b.pos for b in history[0])}
+
+    to_visit = deque([(start, 0)])
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    seen = set()
+
+    found = False
+
+    while to_visit:
+        current_pos, current_step = to_visit.popleft()
+
+        if (current_pos, current_step) in seen:
+            continue
+
+        seen.add((current_pos, current_step))
+
+        if (current_step + 1) not in history:
+            history[current_step +
+                    1] = cycle_grid(history[current_step], max_x, max_y)
+            occupied[current_step +
+                     1] = set(b.pos for b in history[current_step + 1])
+
+        if current_pos not in occupied[current_step + 1]:
+            # wait for one minute if the place is blizzard free the next step
+            to_visit.append((current_pos, current_step + 1))
+
+        for d in deltas:
+            nx, ny = current_pos[0] + d[0], current_pos[1] + d[1]
+
+            if (nx, ny) == target:
+                s1 = current_step+1
+                found = True
+                break
+
+            if 0 < nx < max_x and 0 < ny < max_y:
+                if (nx, ny) not in occupied[current_step + 1]:
+                    to_visit.append(((nx, ny), current_step + 1))
+
+        if found:
+            break
+
+    to_visit = deque([(target, s1)])
+    found = False
+
+    while to_visit:
+        current_pos, current_step = to_visit.popleft()
+
+        if (current_pos, current_step) in seen:
+            continue
+
+        seen.add((current_pos, current_step))
+
+        if (current_step + 1) not in history:
+            history[current_step +
+                    1] = cycle_grid(history[current_step], max_x, max_y)
+            occupied[current_step +
+                     1] = set(b.pos for b in history[current_step + 1])
+
+        if current_pos not in occupied[current_step + 1]:
+            # wait for one minute if the place is blizzard free the next step
+            to_visit.append((current_pos, current_step + 1))
+
+        for d in deltas:
+            nx, ny = current_pos[0] + d[0], current_pos[1] + d[1]
+
+            if (nx, ny) == start:
+                s2 = current_step+1
+                found = True
+                break
+
+            if 0 < nx < max_x and 0 < ny < max_y:
+                if (nx, ny) not in occupied[current_step + 1]:
+                    to_visit.append(((nx, ny), current_step + 1))
+
+        if found:
+            break
+
+    to_visit = deque([(start, s2)])
+    found = False
+
+    while to_visit:
+        current_pos, current_step = to_visit.popleft()
+
+        if (current_pos, current_step) in seen:
+            continue
+
+        seen.add((current_pos, current_step))
+
+        if (current_step + 1) not in history:
+            history[current_step +
+                    1] = cycle_grid(history[current_step], max_x, max_y)
+            occupied[current_step +
+                     1] = set(b.pos for b in history[current_step + 1])
+
+        if current_pos not in occupied[current_step + 1]:
+            # wait for one minute if the place is blizzard free the next step
+            to_visit.append((current_pos, current_step + 1))
+
+        for d in deltas:
+            nx, ny = current_pos[0] + d[0], current_pos[1] + d[1]
+
+            if (nx, ny) == target:
+                print(current_step+1)
+                return
+
+            if 0 < nx < max_x and 0 < ny < max_y:
+                if (nx, ny) not in occupied[current_step + 1]:
+                    to_visit.append(((nx, ny), current_step + 1))
 
 
 if __name__ == "__main__":
