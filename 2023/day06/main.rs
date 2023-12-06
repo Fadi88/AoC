693f5eab810a7a11536e79fd8926680c07d26bbe
Fadi Sedik <fady88@gmail.com>
2023-12-06 07:30:09 +0100

    adding day06 2023 both parts in rust

--- 2023/day06/main.rs
+++ 2023/day06/main.rs
@@ -1,5 +1,4 @@
 use std::time;
-use std::fs;
 
 fn bench(f: fn()) {
     let t0 = time::Instant::now();
@@ -10,11 +9,57 @@ fn bench(f: fn()) {
 }
 
 fn part_1() {
-    fs::read_to_string("template/input.txt").unwrap().split("\n");
+    let mut input: Vec<Vec<u16>> = Vec::new();
+
+    for l in include_str!("input.txt").split("\n") {
+        let t = l
+            .split_whitespace()
+            .into_iter()
+            .skip(1)
+            .map(|num| num.parse::<u16>().unwrap())
+            .collect::<Vec<_>>();
+        input.push(t);
+    }
+    let mut total = 1u32;
+    for i in 0..input.get(0).unwrap().len() {
+        let tt = input.get(0).unwrap().get(i).unwrap();
+        let d: &u16 = input.get(1).unwrap().get(i).unwrap();
+
+        let mut wins = 0u16;
+        for t in 0..=*tt {
+            if (tt - t) * t > *d {
+                wins += 1;
+            }
+        }
+        total *= wins as u32;
+    }
+    dbg!(total);
 }
 
 fn part_2() {
-    fs::read_to_string("template/input.txt").unwrap().split("\n");
+    let mut input: Vec<u64> = Vec::new();
+    for l in include_str!("input.txt").split("\n") {
+        let t = l
+            .split_whitespace()
+            .into_iter()
+            .skip(1)
+            .collect::<Vec<_>>()
+            .join("");
+        input.push(t.parse::<u64>().unwrap());
+    }
+
+    let mut wins = 0u64;
+
+    let tt = input.get(0).unwrap().clone();
+    let d: u64 = input.get(1).unwrap().clone();
+
+    for t in 0..=tt {
+        if (tt - t) * t > d {
+            wins += 1;
+        }
+    }
+
+    dbg!(wins);
 }
 
 fn main() {
