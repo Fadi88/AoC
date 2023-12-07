287787dc7fa55e7b0e10416efa3bd6815fea31af
Fadi Sedik <fady88@gmail.com>
2023-12-07 18:44:00 +0100

    adding empty templates

---
+++ 2023/day25/code.py
@@ -0,0 +1,27 @@
+from time import perf_counter
+
+
+def profiler(method):
+    def wrapper_method(*arg, **kw):
+        t = perf_counter()
+        ret = method(*arg, **kw)
+        print('Method ' + method.__name__ + ' took : ' +
+              "{:2.5f}".format(perf_counter()-t) + ' sec')
+        return ret
+    return wrapper_method
+
+
+@profiler
+def part1():
+    pass
+
+
+@profiler
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+
+    part1()
+    part2()
