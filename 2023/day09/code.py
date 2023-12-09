3e7ac8dd595990332dbdd659aa527af7d317519b
Fadi Sedik <fady88@gmail.com>
2023-12-09 06:25:01 +0100

    optmize part 2 day09 python

--- 2023/day09/code.py
+++ 2023/day09/code.py
@@ -12,16 +12,16 @@ def profiler(method):
 
 
 def get_next(l):
-    m =[]
+    m = []
     m.append(l)
     while len(set(m[-1])) > 1:
         new_l = []
-        for i in range(1,len(m[-1])):
-            new_l.append(m[-1][i]-m[-1][i-1])
+        for i in range(1, len(m[-1])):
+            new_l.append(m[-1][i] - m[-1][i - 1])
         m.append(new_l)
 
-    for i in reversed(range(len(m)-1)):
-        m[i].append(m[i+1][-1]+m[i][-1])
+    for i in reversed(range(len(m) - 1)):
+        m[i].append(m[i + 1][-1] + m[i][-1])
     return m[0][-1]
 
 
@@ -31,24 +31,12 @@ def part1():
 
     print(sum([get_next(v) for v in oasis]))
 
-def get_prev(l):
-    m =[]
-    m.append(l[::-1])
-    while len(set(m[-1])) > 1:
-        new_l = []
-        for i in range(1,len(m[-1])):
-            new_l.append(m[-1][i]-m[-1][i-1])
-        m.append(new_l)
-
-    for i in reversed(range(len(m)-1)):
-        m[i].append(m[i+1][-1]+m[i][-1])
-    return m[0][-1]
 
 @profiler
 def part2():
     oasis = [list(map(int, l.split())) for l in open("day09/input.txt").read().splitlines()]
 
-    print(sum([get_prev(v) for v in oasis]))
+    print(sum([get_next(v[::-1]) for v in oasis]))
 
 
 if __name__ == "__main__":
