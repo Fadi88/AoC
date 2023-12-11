940fb3740151f354eaab705ef374b480d9371890
Fadi Sedik <fady88@gmail.com>
2023-12-11 07:36:44 +0100

    refactor day 11 2023

--- 2023/day11/code.py
+++ 2023/day11/code.py
@@ -14,72 +14,75 @@ def profiler(method):
     return wrapper_method
 
 
-@profiler
-def part1():
-    galaxies = set()
+def augment_by_n(galaxies, empty_x, empty_y, n):
+    new_galaxies = set()
+    for g in galaxies:
+        n_x = g.real + (n - 1) * sum(list(map(lambda x: x < g.real, empty_x)))
+        n_y = g.imag + (n - 1) * sum(list(map(lambda x: x < g.imag, empty_y)))
 
-    data = open("day11/input.txt").read().splitlines()
-    augemented_x = [[] for _ in range(len(data))]
-
-    for x in range(len(data[0])):
-        if [l[x] for l in data].count("#") == 0:
-            for y in range(len(data)):
-                augemented_x[y] += [".", "."]
-        else:
-            for y in range(len(data)):
-                augemented_x[y].append(data[y][x])
-
-    augemented_y = []
-    for l in augemented_x:
-        augemented_y.append(l)
-        if l.count("#") == 0:
-            augemented_y.append(l)
-
-    for y, l in enumerate(augemented_y):
-        for x, c in enumerate(l):
-            if c == "#":
-                galaxies.add(x + y * 1j)
-
-    dist = []
-    for p1, p2 in combinations(galaxies, 2):
-        dist.append(int(abs(p1.real - p2.real) + abs(p1.imag - p2.imag)))
-
-    print(sum(dist))
+        new_galaxies.add(n_x + n_y * 1j)
+    return new_galaxies
 
 
-@profiler
-def part2():
-    galaxies = set()
-
-    data = open("day11/input.txt").read().splitlines()
-
-    for y, l in enumerate(data):
-        for x, c in enumerate(l):
-            if c == "#":
-                galaxies.add(x + y * 1j)
-
-    empty_y = []
-    for y, l in enumerate(data):
-        if l.count("#") == 0:
-            empty_y.append(y)
-
+def get_empty_x(data):
     empty_x = []
     for x in range(len(data[0])):
         if [l[x] for l in data].count("#") == 0:
             empty_x.append(x)
 
-    new_galaxies = set()
-    for g in galaxies:
-        n_x = g.real + (1000000 - 1) * sum(list(map(lambda x: x < g.real, empty_x)))
-        n_y = g.imag + (1000000 - 1) * sum(list(map(lambda x: x < g.imag, empty_y)))
+    return empty_x
 
-        new_galaxies.add(n_x + n_y * 1j)
 
-    dist = []
-    for p1, p2 in combinations(new_galaxies, 2):
-        dist.append(int(abs(p1.real - p2.real) + abs(p1.imag - p2.imag)))
+def get_empty_y(data):
+    empty_y = []
+    for y, l in enumerate(data):
+        if l.count("#") == 0:
+            empty_y.append(y)
+    return empty_y
 
-    print(sum(dist))
+
+def get_galaxies(data):
+    galaxies = set()
+    for y, l in enumerate(data):
+        for x, c in enumerate(l):
+            if c == "#":
+                galaxies.add(x + y * 1j)
+
+    return galaxies
+
+
+def get_dist(pair):
+    p1 = pair[0]
+    p2 = pair[1]
+    return int(abs(p1.real - p2.real) + abs(p1.imag - p2.imag))
+
+
+@profiler
+def part1():
+    data = open("day11/input.txt").read().splitlines()
+
+    galaxies = get_galaxies(data)
+
+    empty_y = get_empty_y(data)
+    empty_x = get_empty_x(data)
+
+    new_galaxies = augment_by_n(galaxies, empty_x, empty_y, 2)
+
+    print(sum(map(get_dist,combinations(new_galaxies, 2))))
+
+
+@profiler
+def part2():
+    data = open("day11/input.txt").read().splitlines()
+
+    galaxies = get_galaxies(data)
+
+    empty_y = get_empty_y(data)
+    empty_x = get_empty_x(data)
+
+    new_galaxies = augment_by_n(galaxies, empty_x, empty_y, 1000000)
+
+    print(sum(map(get_dist,combinations(new_galaxies, 2))))
 
 
 if __name__ == "__main__":
