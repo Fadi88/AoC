f027b16d15bc6527577e642b96a71a9623012a30
Fadi Sedik <fady88@gmail.com>
2023-12-12 18:59:12 +0100

    day12 2023 python both parts

--- 2023/day12/code.py
+++ 2023/day12/code.py
@@ -1,6 +1,7 @@
 # pylint: disable=C0114,C0116,C0301,C0209,W1514
 
 from time import perf_counter
+from functools import cache
 
 
 def profiler(method):
@@ -13,14 +14,54 @@ def profiler(method):
     return wrapper_method
 
 
+@cache
+def get_comb(pattern, nums):
+    if len(pattern) == 0:
+        return len(nums) == 0
+
+    match pattern[0]:
+        case "?":
+            return get_comb(pattern.replace("?", ".", 1), nums) + get_comb(pattern.replace("?", "#", 1), nums)
+        case ".":
+            return get_comb(pattern.strip("."), nums)
+        case "#":
+            if (
+                len(nums) == 0
+                or len(pattern) < sum(nums)
+                or len(pattern) < nums[0]
+                or pattern[: nums[0]].count(".") > 0
+                or (len(nums) > 1 and len(pattern) < nums[0] + 1)
+                or (len(nums) > 1 and pattern[nums[0]] == "#")
+            ):
+                return 0
+            # nasty nasty nasty bug for the last spring :/ 
+            return get_comb(pattern[nums[0] + (len(nums) > 1) :], nums[1:])
+
+
 @profiler
 def part1():
-    pass
+    total = 0
+
+    for l in open("day12/input.txt"):
+        pattern = l.split()[0]
+        nums = tuple(map(int, l.split()[1].split(",")))
+
+        total += get_comb(pattern, nums)
+
+    print(total)
 
 
 @profiler
 def part2():
-    pass
+    total = 0
+
+    for l in open("day12/input.txt"):
+        pattern = l.split()[0]
+        nums = tuple(map(int, l.split()[1].split(",")))
+
+        total += get_comb("?".join([pattern] * 5), nums * 5)
+
+    print(total)
 
 
 if __name__ == "__main__":
