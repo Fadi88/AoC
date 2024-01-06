180690b7aa956f610ce2667c560187de276ea66c
Fadi Sedik <fady88@gmail.com>
2024-01-06 08:44:47 +0100

    adding day 25 2023 python

--- 2023/day25/code.py
+++ 2023/day25/code.py
@@ -1,27 +1,31 @@
 from time import perf_counter
+import networkx
 
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
         t = perf_counter()
         ret = method(*arg, **kw)
-        print('Method ' + method.__name__ + ' took : ' +
-              "{:2.5f}".format(perf_counter()-t) + ' sec')
+        print("Method " + method.__name__ + " took : " + "{:2.5f}".format(perf_counter() - t) + " sec")
         return ret
+
     return wrapper_method
 
 
 @profiler
 def part1():
-    pass
+    graph = networkx.Graph()
 
+    for l in open("day25/input.txt"):
+        src, dst = l.strip().split(": ")
 
-@profiler
-def part2():
-    pass
+        for n in dst.split():
+            graph.add_edge(src, n)
+
+    _, partitions = networkx.stoer_wagner(graph)
+
+    print(len(partitions[0]) * len(partitions[1]))
 
 
 if __name__ == "__main__":
-
     part1()
-    part2()
