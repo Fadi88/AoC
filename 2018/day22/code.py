a718e6a3d8ff9ad9fc4d260cfbc668c1590f3bea
Fadi Sedik <fady88@gmail.com>
2024-11-02 16:40:10 +0100

    solving day 22 2018 part 2 bug, step was not calucated correctly

--- 2018/day22/code.py
+++ 2018/day22/code.py
@@ -1,3 +1,5 @@
+# pylint: disable=C0114, C0116, C0209
+
 import time
 import heapq
 
@@ -5,27 +7,27 @@ DEPTH = 10647
 TARGET = (7, 770)
 
 # DEPTH = 510
-# TARGET = (10,10)
+# TARGET = (10, 10)
 
 
 def profiler(method):
     def wrapper_method(*arg, **kw):
         t = time.time()
         ret = method(*arg, **kw)
-        print("Method " + method.__name__ + " took : " +
-              "{:2.5f}".format(time.time() - t) + " sec")
+        print("Method " + method.__name__ + " took : " + "{:2.5f}".format(time.time() - t) + " sec")
         return ret
 
     return wrapper_method
 
 
 def get_cave():
-    cave_idx = [[-1] * (TARGET[0]+100) for y in range(TARGET[1] + 100)]
-    cave_ero = [[-1] * (TARGET[0]+100) for y in range(TARGET[1] + 100)]
-    cave_type = [[-1] * (TARGET[0]+100) for y in range(TARGET[1] + 100)]
+    padding = 25
+    cave_idx = [[-1] * (TARGET[0] + padding) for y in range(TARGET[1] + padding)]
+    cave_ero = [[-1] * (TARGET[0] + padding) for y in range(TARGET[1] + padding)]
+    cave_type = [[-1] * (TARGET[0] + padding) for y in range(TARGET[1] + padding)]
 
-    for y in range(TARGET[1] + 100):
-        for x in range(TARGET[0] + 100):
+    for y in range(TARGET[1] + padding):
+        for x in range(TARGET[0] + padding):
             if (x, y) == (0, 0) or (x, y) == TARGET:
                 cave_idx[y][x] = 0
             elif y == 0:
@@ -33,18 +35,17 @@ def get_cave():
             elif x == 0:
                 cave_idx[y][x] = y * 48271
             else:
-                cave_idx[y][x] = cave_ero[y][x-1] * cave_ero[y-1][x]
+                cave_idx[y][x] = cave_ero[y][x - 1] * cave_ero[y - 1][x]
             cave_ero[y][x] = (cave_idx[y][x] + DEPTH) % 20183
             cave_type[y][x] = cave_ero[y][x] % 3
 
-    return (cave_type)
+    return cave_type
 
 
 @profiler
 def part_1():
     cave_type = get_cave()
-    print(sum([cave_type[y][x] for x in range(TARGET[0] + 1)
-          for y in range(TARGET[1] + 1)]))
+    print(sum([cave_type[y][x] for x in range(TARGET[0] + 1) for y in range(TARGET[1] + 1)]))
 
 
 @profiler
@@ -59,7 +60,7 @@ def part_2():
         narrow: [torch, neither],
     }
 
-    start = (0, (0, 0), torch)
+    start = (torch, (0, 0), torch, torch)
     dst = (TARGET, torch)
 
     seen = set()
@@ -68,14 +69,14 @@ def part_2():
     deltas = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
     while to_visit:
-        d, p, t = heapq.heappop(to_visit)
+        d, p, t, pt = heapq.heappop(to_visit)
 
         if (p, t) in seen:
             continue
 
-        if (p, t) == dst:
+        if (p, t) == dst and pt == t:
             print(d)
-            
+            return
 
         seen.add((p, t))
 
@@ -83,12 +84,10 @@ def part_2():
             nx, ny = p[0] + dx, p[1] + dy
 
             if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid):
-                nd = d + 1
                 for nt in allowed_tools[grid[p[1]][p[0]]]:
                     if nt in allowed_tools[grid[ny][nx]]:
-                        if nt != t:
-                            nd += 7
-                    heapq.heappush(to_visit, (nd, (nx, ny), nt))
+                        step = 1 if nt == t else 8
+                        heapq.heappush(to_visit, (d + step, (nx, ny), nt, t))
 
 
 if __name__ == "__main__":
