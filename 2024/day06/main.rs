5450282027f8186f1c4881b6e8d852e71af66b51
Fadi Sedik <fady88@gmail.com>
2024-12-06 05:19:33 +0100

    tempalte for day06 2026

---
+++ 2024/day06/main.rs
@@ -0,0 +1,23 @@
+use std::collections::{HashMap, HashSet};
+use std::time;
+
+fn bench(f: fn()) {
+    let t0 = time::Instant::now();
+    let ret = f();
+    println!("time used {:?}", time::Instant::now().duration_since(t0));
+
+    ret
+}
+
+fn part_1() {
+    let input = include_str!("input.txt");
+}
+
+fn part_2() {
+    let input = include_str!("input.txt");
+}
+
+fn main() {
+    bench(part_1);
+    bench(part_2);
+}
