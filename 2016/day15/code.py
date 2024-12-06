41e7306aeba8899d8018516a708863b104f10e4e
Fadi Sedik <fady88@gmail.com>
2024-12-06 14:46:11 +0100

    adding day15 2016 python

--- 2016/day15/code.py
+++ 2016/day15/code.py
@@ -14,17 +14,33 @@ def profiler(method):
     return wrapper_method
 
 
+def get_nums(l):
+    ps = l.strip().split()
+    return (int(ps[3]), int(ps[-1][:-1]))
+
+
+@profiler
+def part_1():
+    inp = [get_nums(l) for l in open('day15/input.txt').readlines()]
+
+    time = 0
+    while not all((time + i + disc[1]) % disc[0] == 0 for i, disc in enumerate(inp)):
+        time += 1
+
+    print(time-1)
+
+@profiler
+def part_2():
+    inp = [get_nums(l) for l in open('day15/input.txt').readlines()]
+    inp.append((11, 0)) 
+
+    time = 0
+    while not all((time + i + disc[1]) % disc[0] == 0 for i, disc in enumerate(inp)):
+        time += 1
+
+    print(time-1)
+
 if __name__ == "__main__":
 
-    inp = []
-    for l in open('input.txt'):
-        p = l.strip().split()
-        inp.append([int(p[3]), int(p[-1][:-1])])
-
-    m = [l[0] for l in inp]
-    r = [(l[0] - l[1] - (idx + 1))%l[0] for idx,l in enumerate(inp)]
-
-    # https://www.dcode.fr/restes-chinois
-    for p in zip(r , m):
-        print(*p)
-
+    part_1()
+    part_2()
