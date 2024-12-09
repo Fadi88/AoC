5fdc325307ab77f74d6f94d74f5b62e9686aeec5
Fadi Sedik <fady88@gmail.com>
2024-12-09 04:28:39 +0100

    template for day09

---
+++ 2024/day09/main.rs
@@ -0,0 +1,26 @@
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
+use std::time::Instant;
+
+fn bench<F, R>(f: F) -> R
+where
+    F: FnOnce() -> R,
+{
+    let t0 = Instant::now();
+    let result = f(); // Call the function and store the result
+    println!("time used: {:?}", Instant::now().duration_since(t0));
+    result // Return the result of the function
+}
+
+fn part_1() {
+    let input = include_str!("input.txt");
+}
+
+fn part_2() {
+    let input = include_str!("input.txt");
+}
+
+fn main() {
+    bench(part_1);
+    bench(part_2);
+}
