4fe2bdd54f5745509cb750c0fccb46b67d011518
Fadi Sedik <fady88@gmail.com>
2024-12-09 05:48:24 +0100

    update template

--- 2024/template/code.py
+++ 2024/template/code.py
@@ -1,28 +1,35 @@
-# pylint: disable=C0114,C0116,C0301,C0209,W1514
+# pylint: disable=C0114,C0116,C0301,C0209,W1514,C0414
 
-from time import perf_counter
+from time import time as perf_counter
+from typing import Any
+import os
+
+input_file = os.path.join(os.path.dirname(__file__), "input.txt")
+# input_file = os.path.join(os.path.dirname(__file__), "test.txt")
 
 
 def profiler(method):
-    def wrapper_method(*arg, **kw):
+    def wrapper_method(*args: Any, **kwargs: Any) -> Any:
         t = perf_counter()
-        ret = method(*arg, **kw)
-        print("Method " + method.__name__ + " took : " + "{:2.5f}".format(perf_counter() - t) + " sec")
+        ret = method(*args, **kwargs)
+        print(f"Method {method.__name__} took : {perf_counter() - t:.3f} sec")
         return ret
 
     return wrapper_method
 
 
 @profiler
-def part1():
-    pass
+def part_1():
+    with open(input_file) as _f:
+        pass
 
 
 @profiler
-def part2():
-    pass
+def part_2():
+    with open(input_file) as _f:
+        pass
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    part_1()
+    part_2()
