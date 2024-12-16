b6e7856b44ccdd1866b790425066581a4d1dbed3
Fadi Sedik <fady88@gmail.com>
2024-12-16 07:47:12 +0100

    linting

--- 2024/day16/code.py
+++ 2024/day16/code.py
@@ -4,7 +4,6 @@ import heapq
 from typing import Any
 import os
 from time import perf_counter_ns
-from collections import deque
 
 input_file = os.path.join(os.path.dirname(__file__), "input.txt")
 # input_file = os.path.join(os.path.dirname(__file__), "test.txt")
@@ -26,24 +25,7 @@ def profiler(method):
     return wrapper_method
 
 
-@profiler
-def part_1():
-    start = (-1, -1)
-    end = (-1.-1)
-    d = ">"
-    free_spaces = set()
-
-    with open(input_file) as f:
-        for y, l in enumerate(f):
-            for x, c in enumerate(l.strip()):
-                if c == "E":
-                    end = (x, y)
-                    free_spaces.add((x, y))
-                elif c == "S":
-                    start = (x, y)
-                elif c == ".":
-                    free_spaces.add((x, y))
-
+def dijkstra(start, free_spaces):
     deltas = {
         ">": (1, 0),
         "v": (0, 1),
@@ -54,11 +36,11 @@ def part_1():
 
     to_visit = []
     visited = {
-        (start, d): 0,
+        (start, ">"): 0,
 
     }
 
-    heapq.heappush(to_visit, (0, d, start))
+    heapq.heappush(to_visit, (0, ">", start))
 
     while to_visit:
         score, cd, (cx, cy) = heapq.heappop(to_visit)
@@ -81,17 +63,14 @@ def part_1():
                 visited[((cx, cy), nd)] = score + 1000
                 heapq.heappush(to_visit, (score+1000, nd, (cx, cy)))
 
-    print(min(v for k, v in visited.items() if k[0] == end))
+    return visited
 
 
 @profiler
-def part_2():
-    start = (-1, -1)
-    end = (-1.-1)
-    d = ">"
-    free_spaces = set()
+def part_1():
 
-    max_x, max_y = -1, -1
+    start = (-1, -1)
+    free_spaces = set()
 
     with open(input_file) as f:
         for y, l in enumerate(f):
@@ -104,6 +83,11 @@ def part_2():
                 elif c == ".":
                     free_spaces.add((x, y))
 
+    visited = dijkstra(start, free_spaces)
+    print(min(v for k, v in visited.items() if k[0] == end))
+
+
+def back_track(visited, target_state):
     deltas = {
         ">": (1, 0),
         "v": (0, 1),
@@ -111,41 +95,6 @@ def part_2():
         "^": (0, -1),
     }
     rot = ">v<^"
-
-    to_visit = []
-    visited = {
-        (start, d): 0,
-
-    }
-
-    heapq.heappush(to_visit, (0, d, start))
-
-    while to_visit:
-        score, cd, (cx, cy) = heapq.heappop(to_visit)
-
-        if ((cx, cy), cd) in visited and visited[((cx, cy), cd)] < score:
-            continue
-
-        dx, dy = deltas[cd]
-        # try forward
-        if (cx+dx, cy+dy) in free_spaces:
-            np = (cx+dx, cy+dy)
-            if (np, cd) not in visited or visited[(np, cd)] > score + 1:
-                visited[(np, cd)] = score + 1
-                heapq.heappush(to_visit, (score+1, cd, np))
-
-        # try turn
-        for dr in [-1, 1]:
-            nd1 = rot[(rot.index(cd) + dr) % 4]
-            if ((cx, cy), nd1) not in visited or visited[((cx, cy), nd1)] > score + 1000:
-                visited[((cx, cy), nd1)] = score + 1000
-                heapq.heappush(to_visit, (score+1000, nd1, (cx, cy)))
-
-    # back track from end with lowest cost
-    target_score = min(v for k, v in visited.items() if k[0] == end)
-    target_state = [k for k, v in visited.items() if v ==
-                    target_score and k[0] == end][0]
-
     to_visit = [(target_state)]
 
     seen = set()
@@ -167,7 +116,34 @@ def part_2():
         if (cp, nd2) in visited and visited[(cp, nd2)] + 1000 == visited[(cp, cd)]:
             to_visit.append((cp, nd2))
 
-    print(len(seen))
+    return seen
+
+
+@profiler
+def part_2():
+
+    start = (-1, -1)
+    free_spaces = set()
+
+    with open(input_file) as f:
+        for y, l in enumerate(f):
+            for x, c in enumerate(l.strip()):
+                if c == "E":
+                    end = (x, y)
+                    free_spaces.add((x, y))
+                elif c == "S":
+                    start = (x, y)
+                elif c == ".":
+                    free_spaces.add((x, y))
+
+    visited = dijkstra(start, free_spaces)
+
+    # back track from end with lowest cost
+    target_score = min(v for k, v in visited.items() if k[0] == end)
+    target_state = [k for k, v in visited.items() if v ==
+                    target_score and k[0] == end][0]
+
+    print(len(back_track(visited, target_state)))
 
 
 if __name__ == "__main__":
