136b3da36a5bcc7dcc2f09bbf7e2430c59da83c1
Fadi Sedik <fady88@gmail.com>
2024-12-19 06:41:59 +0100

    adding day19 2024 python

--- 2024/day19/code.py
+++ 2024/day19/code.py
@@ -3,6 +3,7 @@
 from typing import Any
 import os
 from time import perf_counter_ns
+from collections import defaultdict
 
 input_file = os.path.join(os.path.dirname(__file__), "input.txt")
 # input_file = os.path.join(os.path.dirname(__file__), "test.txt")
@@ -24,16 +25,46 @@ def profiler(method):
     return wrapper_method
 
 
+count = {}
+
+
+def count_allowed(towel, patterns):
+    combs = 0
+    if towel in count:
+        return count[towel]
+
+    for p in patterns:
+        if towel == p:
+            combs += 1
+        if towel.startswith(p):
+            new_towel = towel.replace(p, "", 1)
+            combs += count_allowed(new_towel, patterns)
+    count[towel] = combs
+    return combs
+
+
 @profiler
 def part_1():
-    with open(input_file) as _f:
-        pass
+    with open(input_file) as f:
+        ps = f.read().split("\n\n")
+
+    allowed = list(ps[0].split(", "))
+    towels = ps[1].split()
+
+    print(sum(count_allowed(t, allowed) > 0 for t in towels))
 
 
 @profiler
 def part_2():
-    with open(input_file) as _f:
-        pass
+    with open(input_file) as f:
+        ps = f.read().split("\n\n")
+
+    allowed = list(ps[0].split(", "))
+    allowed.sort(key=lambda x: -len(x))
+
+    towels = ps[1].split()
+
+    print(sum(count_allowed(t, allowed) for t in towels))
 
 
 if __name__ == "__main__":
