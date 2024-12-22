3987b3add2035188b0d96812b20e1f378dff7d55
Fadi Sedik <fady88@gmail.com>
2024-12-22 07:09:44 +0100

    adding day 22 2024 in python

--- 2024/day22/code.py
+++ 2024/day22/code.py
@@ -3,6 +3,7 @@
 from typing import Any
 import os
 from time import perf_counter_ns
+from collections import defaultdict
 
 input_file = os.path.join(os.path.dirname(__file__), "input.txt")
 # input_file = os.path.join(os.path.dirname(__file__), "test.txt")
@@ -24,16 +25,63 @@ def profiler(method):
     return wrapper_method
 
 
+def gen_new(n):
+    n ^= n * 64
+    n %= 16777216
+
+    n ^= n // 32
+    n %= 16777216
+
+    n ^= n * 2048
+    n %= 16777216
+
+    return n
+
+
 @profiler
 def part_1():
-    with open(input_file) as _f:
-        pass
+    with open(input_file) as f:
+        data = list(map(int, f.read().splitlines()))
+
+    for _ in range(2000):
+        data = [gen_new(n) for n in data]
+
+    print(sum(data))
+
+
+def get_deltas(n):
+    deltas = []
+
+    for _ in range(2000):
+        new_n = gen_new(n)
+
+        b = n % 10
+        nb = new_n % 10
+
+        deltas.append((nb-b, nb))
+
+        n = new_n
+
+    return deltas
 
 
 @profiler
 def part_2():
-    with open(input_file) as _f:
-        pass
+    with open(input_file) as f:
+        data = list(map(int, f.read().splitlines()))
+
+    patterns_roi = defaultdict(int)
+
+    for init in data:
+        deltas = get_deltas(init)
+        added = set()
+        for idx in range(len(deltas) - 4):
+            pat = tuple(d[0] for d in deltas[idx:idx+4])
+            if pat not in added:
+                patterns_roi[tuple(pat)] += deltas[idx+3][1]
+                added.add(pat)
+
+    print(max(patterns_roi.values()))
 
 
 if __name__ == "__main__":
