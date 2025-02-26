fabd3fdb2adc79bdd2faafd4131834f0fcf7d9ce
Fadi Sedik <fady88@gmail.com>
2025-02-26 11:05:47 +0100

    adding code for part2 2016 day22

--- 2016/day22/code.py
+++ 2016/day22/code.py
@@ -20,13 +20,14 @@ def part1():
     for l in open('input.txt').read().split('\n')[2:]:
         p = l.split()
 
-        g = re.findall(r'-x(\d+)-y(\d+)',l)
-        grid[(int(g[0][0]),int(g[0][1]))] = [int(p[1][:-1]) , int(p[2][:-1])]
+        g = re.findall(r'-x(\d+)-y(\d+)', l)
+        grid[(int(g[0][0]), int(g[0][1]))] = [int(p[1][:-1]), int(p[2][:-1])]
 
     cnt = 0
     for i in grid:
         for o in grid:
-            if o == i : continue
+            if o == i:
+                continue
             a = grid[i]
             b = grid[o]
 
@@ -36,36 +37,52 @@ def part1():
     print(cnt)
 
 
-
-
 @profiler
 def part2():
+    nodes = {}
+    with open('input.txt') as f:
+        lines = f.read().splitlines()[2:]
 
-    grid = {}
-    for l in open('input.txt').read().split('\n')[2:]:
-        p = l.split()
+    for line in lines:
+        parts = line.split()
+        coords = re.findall(r'-x(\d+)-y(\d+)', line)
+        x, y = int(coords[0][0]), int(coords[0][1])
+        nodes[(x, y)] = [int(parts[1][:-1]), int(parts[2][:-1])]
 
-        g = re.findall(r'-x(\d+)-y(\d+)',l)
-        grid[(int(g[0][0]),int(g[0][1]))] = [int(p[1][:-1]) , int(p[2][:-1])]
+    max_x = max(x for x, _ in nodes)
+    max_y = max(y for _, y in nodes)
 
+    empty_node = None
+    node_grid = [['#' if nodes[(x, y)][0] > 100 else '.' for x in range(
+        max_x + 1)] for y in range(max_y + 1)]
 
-    for y in range(31):
-        for x in range(33):
-            if x == y == 0:
-                c = '!'
-            elif y == 0 and x == 32:
-                c = 'G'
-            else :
-                a = grid[(x,y)]
-                if a[1] == 0:
-                    c = '_'
-                elif a[1] > 100:
-                    c = '#'
-                else :
-                    c = '.'
+    for (x, y), node in nodes.items():
+        if node[1] == 0:
+            empty_node = (x, y)
+            break
 
-            print(c,end=' ')
-        print()
+    if empty_node is None:
+        raise ValueError("No empty node found")
+
+    queue = [(empty_node, (max_x, 0), 0)]
+    visited = set()
+    visited.add((empty_node, (max_x, 0)))
+
+    while queue:
+        empty, goal, steps = queue.pop(0)
+
+        if goal == (0, 0):
+            print(steps)
+            break
+
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            new_empty = (empty[0] + dx, empty[1] + dy)
+            if 0 <= new_empty[0] <= max_x and 0 <= new_empty[1] <= max_y and node_grid[new_empty[1]][new_empty[0]] != '#':
+                new_goal = goal if new_empty != goal else empty
+
+                if (new_empty, new_goal) not in visited:
+                    visited.add((new_empty, new_goal))
+                    queue.append((new_empty, new_goal, steps + 1))
 
 
 if __name__ == "__main__":
