533a5ab127e369830777c431787f435d803b9698
Fadi Sedik <fady88@gmail.com>
2025-12-08 04:24:20 +0100

    adding template  for day 08 2025

---
+++ 2025/days/day08/benches/bench.rs
@@ -0,0 +1,13 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use day08::{part_1, part_2};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let input_path = concat!(env!("CARGO_MANIFEST_DIR"), "/input.txt");
+    let input = utils::read_input_from_file(input_path).expect("Failed to read input");
+
+    c.bench_function("part_1", |b| b.iter(|| part_1(black_box(&input))));
+    c.bench_function("part_2", |b| b.iter(|| part_2(black_box(&input))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
