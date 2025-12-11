5979a884b091ac33bd5ecac8a1c41543b74ae85d
Fadi Sedik <fady88@gmail.com>
2025-12-11 06:17:30 +0100

    adding day 11 2025 in python

--- 2025/days/day11/solution.py
+++ 2025/days/day11/solution.py
@@ -5,6 +5,8 @@ Advent of Code 2025 - Day 11
 import os
 import time
 import functools
+from collections import defaultdict
+from functools import cache
 
 # pylint: disable=fixme
 
@@ -42,23 +44,57 @@ def read_input() -> str:
         return f.read().strip()
 
 
+grid = defaultdict(list)
+
+
+@cache
+def count_paths(curr, target):
+    """Count paths from curr to target using DFS with memoization."""
+    if curr == target:
+        return 1
+    total = 0
+    for nxt in grid[curr]:
+        total += count_paths(nxt, target)
+
+    return total
+
+
+def parse_input(data: str):
+    """Parse input data into the global grid."""
+    grid.clear()
+    for l in data.splitlines():
+        ps = l.split(": ")
+        grid[ps[0]].extend(ps[1].split())
+
+
 @timer
-def part_1(data: str) -> int:
+def part_1(_data: str) -> int:
     """Calculate the solution for Part 1."""
-    # TODO: Solve Part 1
-    return len(data)
+    return count_paths("you", "out")
 
 
 @timer
-def part_2(data: str) -> int:
+def part_2(_data: str) -> int:
     """Calculate the solution for Part 2."""
-    # TODO: Solve Part 2
-    return len(data)
+    p1 = (
+        count_paths("svr", "dac")
+        * count_paths("dac", "fft")
+        * count_paths("fft", "out")
+    )
+
+    p2 = (
+        count_paths("svr", "fft")
+        * count_paths("fft", "dac")
+        * count_paths("dac", "out")
+    )
+
+    return p1 + p2
 
 
 def main():
     """Execute the solution for both parts."""
     input_data = read_input()
+    parse_input(input_data)
     part_1(input_data)
     part_2(input_data)
 
